c6dc97a100ff9625f86668ed911fe00eb3eb6ffe
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-10-17 18:46:05 +0200

    Update connect.php

--- siteassociation/connect.php
+++ siteassociation/connect.php
@@ -28,8 +28,8 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 
-  <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="./assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
 </head>
 
 <?php
