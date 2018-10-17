0f92487deaa30e258fe97e9ddf525959f935b0c9
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-10-17 18:50:19 +0200

    Update connect.php

--- siteassociation/connect.php
+++ siteassociation/connect.php
@@ -26,7 +26,7 @@
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"/>
 
   <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
   <link href="assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
