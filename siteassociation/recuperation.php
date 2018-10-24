eda460e112f07c28f5baf1c1999a3ebaf17f5220
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-10-24 22:41:08 +0200

    Update siteassociation/recuperation.php

--- siteassociation/recuperation.php
+++ siteassociation/recuperation.php
@@ -203,21 +203,22 @@ require_once('includes/header.php');
               </div>
 
                     <?php if(isset($error)) { echo '<div class="container">
-                         <div class="row">
-                    <div class="col-md-14">
-                            <div class="alert alert-warning">
-                               <div class="alert-icon">
-                                 <i class="now-ui-icons ui-1_bell-53"></i>
-                               </div>
-                               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                 <span aria-hidden="true"><i class="now-ui-icons ui-1_simple-remove"></i></span>
-                               </button>
-                             <center>'.$error.'</center>
-                          </div>
-                          </div>
+                       <div class="row">
+                  <div class="col-md-14">
+                          <div class="alert alert-warning">
+                             <div class="alert-icon">
+                               <i class="now-ui-icons ui-1_bell-53"></i>
+                             </div>
+                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                               <span aria-hidden="true"><i class="now-ui-icons ui-1_simple-remove"></i></span>
+                             </button>
+                  <div align="center">
+                              <?php $error; ?>
+                  </div>
                           </div>
                        </div>
-                    </div>'; }
+                    </div>
+                  </div>'; }
 
                     ?>
 
