53c06eb44783006f1f45432cb3ad09acb94ed8a8
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-10-24 22:26:54 +0200

    Update siteassociation/recuperation.php

--- siteassociation/recuperation.php
+++ siteassociation/recuperation.php
@@ -121,7 +121,7 @@ if(isset($_POST['recup_submit'],$_POST['recup_mail'])) {
    }
 }
 if(isset($_POST['verif_submit'],$_POST['verif_code'])) {
-   if(!empty($_POST['verif_code'])) {
+   if(!empty($_POST['verif_code'])){
       $verif_code = htmlspecialchars($_POST['verif_code']);
       $verif_req = $db->prepare('SELECT id FROM recuperation WHERE email = ? AND code = ?');
       $verif_req->execute(array($_SESSION['recup_mail'],$verif_code));
@@ -133,13 +133,11 @@ if(isset($_POST['verif_submit'],$_POST['verif_code'])) {
          $up_req->execute(array($_SESSION['recup_mail']));
 
          header('Location:https://jam-mdm.fr/recuperation.php?section=changemdp');
-      }}else {
+      }}}}else{
          $error = "Code invalide ou captcha non coché";
       }
-   } else {
-      $error = "Veuillez entrer votre code de confirmation";
-   }
-}
+
+
 if(isset($_POST['change_submit'])) {
    if(isset($_POST['change_mdp'],$_POST['change_mdpc'])) {
       $verif_confirme = $db->prepare('SELECT confirme FROM recuperation WHERE email = ?');
