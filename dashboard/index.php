6f421fe294933ad94388da0c464279e16af6f4f2
Perez <perez40390@hotmail.fr>
2018-10-28 18:08:20 +0100

    Dashboard vide à faire

--- dashboard/index.php
+++ dashboard/index.php
@@ -42,12 +42,12 @@ require_once('includes/checkconnection.php');
     <div class="wrapper">
         <div class="sidebar" data-active-color="blue" data-background-color="black" data-image="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/worldfires-08232018.jpg">
             <div class="logo">
-                <a href="https://sdedikool.me/" class="simple-text">
-                    SdediKool             </a>
+                <a href="https://jam-mdm.fr/" class="simple-text">
+                    JAM          </a>
             </div>
             <div class="logo logo-mini">
-                <a href="https://sdedikool.me/" class="simple-text">
-                    SK
+                <a href="https://jam-mdm.fr" class="simple-text">
+                    JAM
                 </a>
             </div>
             <div class="sidebar-wrapper">
@@ -79,57 +79,46 @@ foreach($req as $row)
                 </div>
                 <ul class="nav">
                     <li class="active">
-                        <a href="https://dashboard.sdedikool.me/">
+                        <a href="https://dashboard.jam-mdm.fr/">
                             <i class="material-icons">home</i>
-                            <p>Tableau de bord</p>
+                            <p>TAB 1</p>
                         </a>
                     </li>
                     <li >
-                        <a href="my_seedbox.php">
+                        <a href="TAB 1.php">
                             <i class="material-icons">dns</i>
-                            <p>Mes seedbox</p>
+                          <p>TAB 1</p>
                         </a>
                     </li>
                     <li>
-                        <a href="my_vpn.php">
+                        <a href="TAB 1.php">
                             <i class="material-icons">dns</i>
-                            <p>Mes VPN</p>
+                            <p>TAB 1</p>
                         </a>
                     </li>
                                         <li >
-                        <a href="my_bank.php">
+                        <a href="TAB 1.php">
                             <i class="material-icons">account_balance</i>
-                            <p>Banque</p>
+                            <p>TAB 1</p>
                         </a>
                     </li>
                     <li >
-                        <a href="my_space.php">
+                        <a href="TAB 1.php">
                             <i class="material-icons">home</i>
-                            <p>Mon compte</p>
+                            <p>TAB 1</p>
                         </a>
                     </li>
                     <li>
-                        <a href="my_stream.php">
+                        <a href="TAB 1.php">
                             <i class="material-icons">play_arrow</i>
-                            <p>Streaming</p>
+                            <p>TAB 1</p>
                         </a>
                     </li>
-                    <li >
-                        <a href="my_help.php">
-                            <i class="material-icons">help</i>
-                            <p>Assistance</p>
-                        </a>
-                    </li>
-                    <li >
-                        <a href="settings.php">
-                            <i class="material-icons">settings</i>
-                            <p>Paramètres</p>
-                        </a>
-                    </li>
+
                     <li class="active" >
-                        <a href="https://sdedikool.me/panier.php">
+                        <a href="autre.php">
                             <i class="material-icons">store</i>
-                            <p>Retour Panier</p>
+                            <p>Autre</p>
                         </a>
                     </li>
                     <li>
@@ -157,7 +146,7 @@ foreach($req as $row)
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="https://dashboard.sdedikool.me/my_seedbox.php"> Mes seedbox </a>                    </div>
+                        <a class="navbar-brand" href="https://dashboard.jam-mdm.fr/"> Dashboard </a>                    </div>
                     <div class="collapse navbar-collapse">
                         <ul class="nav navbar-nav navbar-right">
                             <li class="dropdown">
@@ -178,211 +167,6 @@ foreach($req as $row)
                     </div>
                 </div>
             </nav>
-
-
-
-            <div class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                                <div class="card-header" data-background-color="orange">
-                                    <i class="material-icons">dns</i>
-                                </div>
-                                <div class="card-content">
-                                    <p class="category">Nombre de services</p>
-                                                                        <h3 class="card-title">
-
-<?php
-            $req = $db->query("SELECT  COUNT(*) as id FROM products_transactions WHERE user_id = '$user_id'");
-
-            $donnees = $req->fetch();
-            $req->closeCursor();
-            echo $donnees['id'];?>
-
-
-                                                                        </h3>
-                                </div>
-                                <div class="card-footer">
-                                    <div class="stats">
-                                       <i class="material-icons text-danger">shopping_cart</i>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                                <div class="card-header" data-background-color="green">
-                                    <i class="material-icons">euro_symbole</i>
-                                </div>
-                                <div class="card-content">
-                                    <p class="category">Solde</p>
-                                    <h3 class="card-title">
-
-<?php
-$user_id = $_SESSION['user_id'];
-$select = $db->query("SELECT * FROM users WHERE id = '$user_id'");
-
-while($s = $select->fetch(PDO::FETCH_OBJ)){
-    ?>
-    <?php echo $s->solde.€; ?>
-
-
-
-
-    <?php
-}
-
-?>
-
-                                    </h3>
-                                </div>
-                                <div class="card-footer">
-                                    <div class="stats">
-                                        <i class="material-icons">add</i>Dernier ajout le :
-
-
-
-<?php
-$user_id = $_SESSION['user_id'];
-$sql = "SELECT DISTINCT date FROM transactions WHERE date= (SELECT MAX(date) FROM transactions where user_id='$user_id') AND user_id = '$user_id'";
-$req = $db->query($sql);
-$req->setFetchMode(PDO::FETCH_ASSOC);
-
-foreach($req as $row)
-{
-    echo $row['date'];
-}
-
-?>
-
-                                                                       </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="card card-stats">
-                                <div class="card-header" data-background-color="red">
-                                    <i class="material-icons">assignment_late</i>
-                                </div>
-                                <div class="card-content">
-                                    <p class="category">Factures</p>
-                                <h3 class="card-title">
-
-            <?php
-            $req = $db->query("SELECT  COUNT(*) as id FROM transactions WHERE user_id = '$user_id'");
-
-            $donnees = $req->fetch();
-            $req->closeCursor();
-            echo $donnees['id'];?>
-
-
-                                                                        </h3>
-                                </div>
-                                <div class="card-footer">
-                                    <div class="stats">
-                                        <i class="material-icons">date_range</i> Dernière facture :
-
-
-
-
-<?php
-$user_id = $_SESSION['user_id'];
-$sql = "SELECT DISTINCT transaction_id FROM transactions WHERE date= (SELECT MAX(date) FROM transactions where user_id='$user_id') AND user_id = '$user_id'";
-$req = $db->query($sql);
-$req->setFetchMode(PDO::FETCH_ASSOC);
-
-foreach($req as $row)
-{
-    echo $row['transaction_id'];
-
-}
-
-?>
-
-
-                                                                     </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
-
-
-
-
-
-
-                <?php
-                    $user_id = $_SESSION['user_id'];
-                    $select = $db->query("SELECT * FROM products_transactions WHERE user_id = '$user_id'");
-
-                    while($s = $select->fetch(PDO::FETCH_OBJ)){
-
-                        ?>
-                        <div class="media-footer">
-                     <a href="my_seedbox.php" class="btn btn-primary btn-wd pull-right">Commande
-                        <?php echo $s->product; ?>
-                        <?php echo $s->status; ?>
-
-
-                          </a>
-                        </div>
-
-
-
-                        <?php
-                    }
-
-                ?>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-header card-header-icon" data-background-color="blue">
-                                    <i class="material-icons">new_releases</i>
-                                </div>
-                                <div class="card-content">
-                                    <h4 class="card-title">Actualités</h4>
-                                                                            <div class="row">
-                                            <blockquote>
-                                                <p>
-
-
-                           <?php
-                            $user_id = $_SESSION['user_id'];
-                            $select = $db->query("SELECT DISTINCT * FROM actus WHERE date= (SELECT MAX(date) FROM actus)");
-
-                            while($s = $select->fetch(PDO::FETCH_OBJ)){
-                                ?>
-                                <?php echo $s->description; ?>
-                                 <?php
-                            }
-
-                            ?>
-                              </p>
-                               <small>
-                               Publié par <?php
-                               $select = $db->query("SELECT * FROM actus WHERE date= (SELECT MAX(date) FROM actus)");
-
-                               while($s = $select->fetch(PDO::FETCH_OBJ)){
-                                ?>
-                            <?php echo $s->auteur; ?>
-
-                              -    <?php echo $s->date; ?>    </small>
-                              </blockquote>
-                            </div>
-                          </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
 
 <?php
 }
