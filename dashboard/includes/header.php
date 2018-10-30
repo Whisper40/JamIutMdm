75f801f5677f84b62c9d7deda57fa0eeb9ec86a1
Paul Boussard <boussard.paul@hotmail.com>
2018-10-30 21:37:25 +0100

    test3 icon

--- dashboard/includes/header.php
+++ dashboard/includes/header.php
@@ -48,7 +48,7 @@ while($unecat = $cat->fetch(PDO::FETCH_OBJ)){
   >
   <?php } ?>
       <a href="<?php echo $unecat->page;?>">
-          <i class="material-icons">person</i>
+          <i class="material-icons"><?php echo $unecat->icon;?></i>
           <p><?php echo $unecat->name;?></p>
       </a>
   </li>
