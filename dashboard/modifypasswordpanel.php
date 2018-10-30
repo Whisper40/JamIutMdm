cf3c427071661ca00e0c3feab07173ecc39b9781
Perez <perez40390@hotmail.fr>
2018-10-30 11:45:35 +0100

    Modif password dashboard

--- dashboard/modifypasswordpanel.php
+++ dashboard/modifypasswordpanel.php
@@ -1,48 +1,61 @@
 
 <?php
 require_once('includes/head.php');
-        
-        $email = $_POST['email']; 
+
+        $email = $_POST['email'];
         $password = $_POST['password'];
         $repeatpassword = $_POST['repeatpassword'];
-        $param_password = password_hash($password, PASSWORD_DEFAULT); 
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
 
         if(!empty($email)&&(!empty($password))&&(!empty($repeatpassword))){
             if($password==$repeatpassword){
                  if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{10,}$#', $password)){
                 $user_id = $_SESSION['user_id'];
-                date_default_timezone_set('Europe/Paris'); 
+                date_default_timezone_set('Europe/Paris');
                 setlocale(LC_TIME, 'fr_FR.utf8','fra');
                 $date = strftime('%Y/%m/%d %H:%M:%S');
                 $newmdp = $db->prepare("UPDATE users SET password=:param_password WHERE id='$user_id' and email='$email'");
                 $newmdp->execute(array(
 
-                    
+
                     "param_password"=>$param_password,
-                    
+
                     )
                 );
 
 
-               
-               
-                
+
+
+
 			?>
-			<script> 
-			
+			<script>
+
           demo.showSwal('success-message');
-          demo.showNotification('top','right','<b>Succès</b> - Modification effectuée !');  
+          demo.showNotification('top','right','<b>Succès</b> - Modification effectuée !');
 
 			</script>
 			<?php
 
 
-                
-            }}else{
+    }else{
+        ?>
+
+
+  <script>
+            demo.showSwal('danger-message');
+            demo.showNotification('top','right','<b>Erreur</b> - Modification non effectuée !');
+            </script>
+
+
+<?php
+    }
+
+
+            }else{
                 ?>
 
 
-					<script> 
+					<script>
                     demo.showSwal('danger-message');
                     demo.showNotification('top','right','<b>Erreur</b> - Modification non effectuée !');
                     </script>
@@ -50,6 +63,5 @@ require_once('includes/head.php');
 
 			<?php
             }}
- 
-        ?>
 
+        ?>
