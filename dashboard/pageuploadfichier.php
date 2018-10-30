d11347a3a6f7f96d72481bfdaf65c050109960da
Perez <perez40390@hotmail.fr>
2018-10-30 16:03:37 +0100

    amelioration

--- dashboard/pageuploadfichier.php
+++ dashboard/pageuploadfichier.php
@@ -127,8 +127,11 @@ if(isset($_POST['submit'])){
       }
 
 
-
-$target_file = $target_dirnew . basename($_FILES["fileToUpload"]["name"]);
+      date_default_timezone_set('Europe/Paris');
+      setlocale(LC_TIME, 'fr_FR.utf8','fra');
+      $date = strftime('%d/%m/%Y %H:%M:%S');
+$target_file_temp = $target_dirnew . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_file_temp.$date;
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
