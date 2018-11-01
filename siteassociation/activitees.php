34cf0355e71ef56eba3d1ea04339f52edcb1c636
Perez <perez40390@hotmail.fr>
2018-11-01 17:31:31 +0100

    retour

--- siteassociation/activitees.php
+++ siteassociation/activitees.php
@@ -9,7 +9,17 @@ require_once('includes/header.php');
 		$select->execute();
 		$s = $select->fetch(PDO::FETCH_OBJ);
 
-	
+		$containsProduct = false;
+		/*
+		if(isset($_SESSION['panier'])){
+			for($i = 0; $i < count($_SESSION['panier']['slugProduit']); $i++){
+	     		if($_SESSION['panier']['slugProduit'][$i] == $s->slug){
+	     			$containsProduct = true;
+	     			break;
+	     		}
+	   		}
+	   	}
+			*/
 
 		$description = $s->description;
 		$description_finale=wordwrap($description,100,'<br />', false); // False sert a dire si on découpe le mot ou non
@@ -20,7 +30,7 @@ require_once('includes/header.php');
 		<h1><?php echo $s->title; ?></h1>
 		<h5><?php echo $description_finale; ?></h5>
 		<h5>Stock : <?php echo $s->stock; ?></h5>
-		<?php if ($s->stock>0){ ?><a href="panier.php?action=ajout&amp;l=<?php echo $s->slug; ?>&amp;q=1&amp;p=<?php echo $s->price; ?>">Je Participe !</a><?php }else{echo'<h5 style="color:red;">Stock épuisé !</h5>';}
+		<?php if(!$containsProduct){ if ($s->stock>0){ ?><a href="panier.php?action=ajout&amp;l=<?php echo $s->slug; ?>&amp;q=1&amp;p=<?php echo $s->price; ?>">Je Participe !</a><?php }else{echo'<h5 style="color:red;">Stock épuisé !</h5>';} } else{ ?><p style="color:green;">Produit déjà ajouté</p><?php }
    		?>
 		</div><br/>
 
@@ -64,7 +74,7 @@ require_once('includes/header.php');
 			<h4><?php echo $s->final_price; ?> €</h4>
 			<h5>Places restantes : <?php echo $s->stock; ?></h5>
 			<!-- Fonction contains à gercler une fois finis-->
-			<?php if ($s->stock>0){ ?><a href="?showmethisactivity=<?php echo $s->slug; ?>&amp;p=<?php echo $s->price; ?>">Voir l'activité</a><?php }else{echo'<h5 style="color:red;">Stock épuisé !</h5>';}
+			<?php if(!$containsProduct){ if ($s->stock>0){ ?><a href="?showmethisactivity=<?php echo $s->slug; ?>&amp;p=<?php echo $s->price; ?>">Voir l'activité</a><?php }else{echo'<h5 style="color:red;">Stock épuisé !</h5>';} } else{ ?><p style="color:green;">Produit déjà ajouté</p><?php }
    		?>
 
 			<br/><br/>
