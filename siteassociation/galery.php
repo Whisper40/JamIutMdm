5eb80a82142f37b0a7b3e3a16e10eaad3f076b61
Perez <perez40390@hotmail.fr>
2018-11-01 22:17:08 +0100

    gallery test 2

--- siteassociation/galery.php
+++ siteassociation/galery.php
@@ -3,25 +3,7 @@ require_once('includes/head.php');
 require_once('includes/header.php');
  ?>
 
- <div class="gallery">
-    <?php
-    // Include database configuration file
 
-
-    // Retrieve images from the database
-    $query = $db->query("SELECT * FROM images WHERE status = 1 ORDER BY uploaded_on DESC");
-
-    if($query->num_rows > 0){
-        while($row = $query->fetch_assoc()){
-            $imageThumbURL = 'assets/images/thumb/'.$row["file_name"];
-            $imageURL = 'assets/images/'.$row["file_name"];
-    ?>
-        <a href="<?php echo $imageURL; ?>" data-fancybox="gallery" data-caption="<?php echo $row["title"]; ?>" >
-            <img src="<?php echo $imageThumbURL; ?>" alt="" />
-        </a>
-    <?php }
-    } ?>
-</div>
 
 <!-- Fancybox CSS library -->
 <link rel="stylesheet" type="text/css" href="includes/fancybox/jquery.fancybox.css">
@@ -32,17 +14,16 @@ require_once('includes/header.php');
 <!-- Fancybox JS library -->
 <script src="includes/fancybox/jquery.fancybox.js"></script>
 
+ <a id="iframe" href="http://www.google.com/">Lien vers une iframe</a>
 <script>
-$("[data-fancybox]").fancybox();
+$(document).ready(function() {
+    $("#iframe").fancybox({
+        'width'             : '75%',
+        'height'            : '75%',
+        'autoScale'         : false,
+        'transitionIn'      : 'elastic',
+        'transitionOut'     : 'elastic',
+        'type'              : 'iframe'
+    });
+});
 </script>
-
-<style>
-
-.gallery img {
-    width: 20%;
-    height: auto;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: .3s;
-}
-</style>
