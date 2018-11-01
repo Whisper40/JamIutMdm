0e610a8c5a17c3588a975e245e35a9094b4fbd60
Perez <perez40390@hotmail.fr>
2018-11-01 22:37:40 +0100

    gallery test 3

--- siteassociation/galery.php
+++ siteassociation/galery.php
@@ -14,16 +14,32 @@ require_once('includes/header.php');
 <!-- Fancybox JS library -->
 <script src="includes/fancybox/jquery.fancybox.js"></script>
 
- <a id="iframe" href="http://www.google.com/">Lien vers une iframe</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm9.staticflickr.com/8507/8454547519_f8116520e1_b.jpg" title="Ayvalık, Turkey (Nejdet Düzen)">
+	<img src="http://farm9.staticflickr.com/8507/8454547519_f8116520e1_m.jpg" alt="" />
+</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm8.staticflickr.com/7152/6394238505_c94fdd1d89_b.jpg" title="Sicilian Scratches   erice (italianoadoravel on/off coming back)">
+	<img src="http://farm8.staticflickr.com/7152/6394238505_c94fdd1d89_m.jpg" alt="" />
+</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm9.staticflickr.com/8481/8215602321_69d9939b8b_b.jpg" title="The Trail (Msjunior-Check out my galleries)">
+	<img src="http://farm9.staticflickr.com/8481/8215602321_69d9939b8b_m.jpg" alt="" />
+</a>
+<a class="fancybox-thumb" rel="fancybox-thumb" href="http://farm9.staticflickr.com/8200/8220393833_e52cabfe80_b.jpg" title="Trees (Joerg Marx)">
+	<img src="http://farm9.staticflickr.com/8200/8220393833_e52cabfe80_m.jpg" alt="" />
+</a>
 <script>
 $(document).ready(function() {
-    $("#iframe").fancybox({
-        'width'             : '75%',
-        'height'            : '75%',
-        'autoScale'         : false,
-        'transitionIn'      : 'elastic',
-        'transitionOut'     : 'elastic',
-        'type'              : 'iframe'
-    });
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
 });
 </script>
