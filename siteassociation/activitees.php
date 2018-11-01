d9072a61d46b0a524ca58389549686075f984cbe
Perez <perez40390@hotmail.fr>
2018-11-01 17:58:08 +0100

    NE PAS TOUCHER

--- siteassociation/activitees.php
+++ siteassociation/activitees.php
@@ -45,8 +45,8 @@ require_once('includes/header.php');
 
 			?>
 			<br/>
-			<a href="?show=<?php echo $s->slug; ?>"><img src="assets/img/<?php echo $s->slug; ?>.<?php echo $s->formatimg; ?>"/></a>
-			<a href="?show=<?php echo $s->slug; ?>"><h2><?php echo $s->title;?></h2></a>
+			<a href="?showmethisactivity=<?php echo $s->slug; ?>"><img src="assets/img/<?php echo $s->slug; ?>.<?php echo $s->formatimg; ?>"/></a>
+			<a href="?showmethisactivity=<?php echo $s->slug; ?>"><h2><?php echo $s->title;?></h2></a>
 			<h5><?php echo $description_finale; ?></h5>
 			<h4><?php echo $s->final_price; ?> €</h4>
 			<h5>Places restantes : <?php echo $s->stock; ?></h5>
