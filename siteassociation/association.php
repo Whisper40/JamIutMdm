399fd4842f2ef6e7a3c479f2c58a4120abad015a
Paul Boussard <boussard.paul@hotmail.com>
2018-11-02 17:57:09 +0100

    Page présentation asso

---
+++ siteassociation/association.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+  // Connexion à la BDD
+   require_once('includes/head.php');
+   ?>
+
+<head>
+  <meta charset="utf-8" />
+
+  <meta name="Description" content="Association JAM ( Jeunesse Associative Montoise ) - Mont de Marsan">
+  <meta name="Keywords" content="jam, association mont de marsan, iut mont de marsan, iut mdm, uppa">
+  <meta name="Identifier-Url" content="https://jam-mdm.fr">
+  <meta name="Reply-To" content="postmaster@jam-mdm.fr"> <!-- Mail Admin -->
+
+
+  <meta name="Rating" content="general">
+  <meta name="Distribution" content="global">
+  <meta name="Category" content="internet">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>Jam - Présentation de l'association</title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <!-- Les CSS utilisés -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
+
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
+</head>
+
+<body class="landing-page sidebar-collapse">
+
+  <?php
+     require_once('includes/header.php');
+  ?>
+
+
+  <div class="wrapper">
+    <div class="page-header page-header-small">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/bg1.jpg');">
+      </div>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">Présentation de l'Association</h1>
+        </div>
+      </div>
+    </div>
+
+    <?php
+    $asso = $db->query("SELECT * FROM descriptionasso");
+    while($association = $asso->fetch(PDO::FETCH_OBJ)){
+      ?>
+    <div class="col-md-4">
+      <div class="team-player">
+        <img src="./assets/img/membres/<?php echo $unmembre->image ?>" alt="Thumbnail Image" class="rounded-circle img-fluid img-raised">
+        <h4 class="title"><?php echo $unmembre->nom ?></h4>
+        <p class="category text-primary"><?php echo $unmembre->fonction ?></p>
+        <p class="description"><?php echo $unmembre->description ?></p><br>
+      </div>
+    </div>
+
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">Qui sommes nous ?</h2>
+            <h5 class="description"><?php echo $association->description1 ?></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-8 col-md-12">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                  <img class="d-block" src="assets/img/bg1.jpg" alt="First slide">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Nature, United States</h5>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block" src="assets/img/bg3.jpg" alt="Second slide">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Somewhere Beyond, United States</h5>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block" src="assets/img/bg4.jpg" alt="Third slide">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Yellowstone National Park, United States</h5>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <i class="now-ui-icons arrows-1_minimal-left"></i>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <i class="now-ui-icons arrows-1_minimal-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section section-about-us">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 ml-auto mr-auto text-center">
+              <h5 class="description"><?php echo $association->description1 ?></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php
+          }
+          ?>
+  </div>
+
+  <?php
+  require_once('includes/footer.php');
+
+  require_once('includes/javascript.php');
+  ?>
