c9514c4990df6bc6ec55fd7ebb58cb8c02fbb85c
Paul Boussard <boussard.paul@hotmail.com>
2018-11-13 20:22:29 +0100

    index

--- siteassociation/index.php
+++ siteassociation/index.php
@@ -8,15 +8,18 @@
 
 <?php
     require_once('includes/navbar.php');
-?>
+
+    $index = $db->query("SELECT * FROM index");
+    while($lindex = $index->fetch(PDO::FETCH_OBJ)){
+    ?>
 
   <div class="wrapper">
     <div class="page-header clear-filter">
-      <div class="page-header-image" data-parallax="true" style="background-image:url(assets/img/IUTmdm.JPG)">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('assets/img/<?php echo $lindex->img1 ?>');">
       </div>
       <div class="container">
         <div class="content-center brand">
-          <img class="n-logo" src="assets/img/jam3.png" alt="">
+          <img class="n-logo" src="assets/img/<?php echo $lindex->logo1 ?>" alt="">
         </div>
       </div>
     </div>
@@ -26,20 +29,17 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-6 col-md-12">
-              <h2 class="title">INTRODUCTION</h2>
+              <h2 class="title"><?php echo $lindex->titre1 ?></h2>
               <h5 class="description">
-                Et te voilà maintenant en toute fierté devenu(e) étudiant(e) à l’Université, dans toute sa splendeur ! Tu te demandes peut-être beaucoup de questions sur ta nouvelle vie estudiantine. JAM (Jeunesse Associative Montoise) est là pour répondre à tes questions et faciliter ton intégration avec cette petite plaquette qui comporte les éléments essentiels sur ta nouvelle vie !
-                Tu as forcément une idée sur ce que c’est un IUT. Mais saches que ce qu’on te dit manque toujours quelques précisions. Une petite information pour démarrer : Est-ce que tu sais déjà que tu puisses joindre une école d’ingénieur après avoir réussir ton DUT ? Oui oui, une école d’ingénieur et encore plus une parmi les meilleures en France ;-) C’est cool alors l’IUT !
-                Et donc, pour atteindre tel objectif ou n’importe quel autre, il faut faire plus que TRAVAILLER, il faut développer une attitude propice à la réussite !
-                Bienvenue à TOI !
+                <?php echo $lindex->description1 ?>
               </h5>
-              <a href="association.php" class="btn btn-primary btn-round btn-lg">Présentation Association</a>
-              <a href="membre.php" class="btn btn-primary btn-simple btn-round btn-lg">Les membres</a>
+              <a href="<?php echo $lindex->lienbt1 ?>" class="btn btn-primary btn-round btn-lg"><?php echo $lindex->bouton1 ?></a>
+              <a href="<?php echo $lindex->lienbt2 ?>" class="btn btn-primary btn-simple btn-round btn-lg"><?php echo $lindex->bouton2 ?></a>
             </div>
             <div class="col-lg-6 col-md-12">
               <div class="container text-center">
                 <br><br><br><br>
-                <img src="./assets/img/jam-logo.png" alt="">
+                <img src="assets/img/<?php echo $lindex->logo2 ?>" alt="">
               </div>
             </div>
           </div>
@@ -51,8 +51,8 @@
         <div class="container">
           <div class="row justify-content-md-center">
             <div class="text-center col-md-12 col-lg-8">
-              <h3 class="title">Do you love this Bootstrap 4 UI Kit?</h3>
-              <h5 class="description">Cause if you do, it can be yours for FREE. Hit the button below to navigate to Creative Tim or Invision where you can find the kit in HTML or Sketch/PSD format. Start a new project or give an old Bootstrap project a new look!</h5>
+              <h3 class="title"><?php echo $lindex->titre2 ?></h3>
+              <h5 class="description"><?php echo $lindex->description2 ?></h5>
             </div>
           </div>
           <div class="row justify-content-md-center sharing-area text-center">
@@ -71,50 +71,10 @@
         </div>
       </div>
     </div>
-    <!-- Sart Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header justify-content-center">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-              <i class="now-ui-icons ui-1_simple-remove"></i>
-            </button>
-            <h4 class="title title-up">Modal title</h4>
-          </div>
-          <div class="modal-body">
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-            </p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default">Nice Button</button>
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--  End Modal -->
-    <!-- Mini Modal -->
-    <div class="modal fade modal-mini modal-primary" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header justify-content-center">
-            <div class="modal-profile">
-              <i class="now-ui-icons users_circle-08"></i>
-            </div>
-          </div>
-          <div class="modal-body">
-            <p>Always have an access to your profile</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-link btn-neutral">Back</button>
-            <button type="button" class="btn btn-link btn-neutral" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 
   <?php
+}
   require_once('includes/footer.php');
 
   require_once('includes/javascript.php');
