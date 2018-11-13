286009522d4ee66fd71f8599d64ac0aad1467bee
Perez <kevin.perez@etud.univ-pau.fr>
2018-11-13 20:23:48 +0100

    css appliqué aux activite et actus

--- siteassociation/activitees.php
+++ siteassociation/activitees.php
@@ -3,6 +3,36 @@
     $nompage = "Activité / Voyage";
     require_once('includes/head.php');
 ?>
+<style>
+.page-header .page-header-image {
+  position: absolute;
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 80%;
+  z-index: -1;
+}
+
+.page-header .content-center {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  z-index: 2;
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: #FFFFFF;
+  padding: 0 15px;
+  width: 100%;
+  max-width: 880px;
+}
+.section {
+  padding: 0px 0;
+  position: relative;
+  background: #FFFFFF;
+}
+</style>
 
 <body class="landing-page sidebar-collapse">
 
