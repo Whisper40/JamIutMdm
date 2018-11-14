f4e97a6bef9b34a9529663c7685d3f1a27d5ba67
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-11-14 22:00:11 +0100

    ajout quantcast

--- siteassociation/statuts.php
+++ siteassociation/statuts.php
@@ -2,6 +2,7 @@
     require_once('includes/connectBDD.php');
     $nompage = "Statuts";
     require_once('includes/head.php');
+    require_once('includes/quantcast.php');
 ?>
 <style>
 .page-header .page-header-image {
