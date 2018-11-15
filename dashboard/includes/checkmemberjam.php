430e309341724731f2dc8f4e2427c773883ca858
Perez <kevin.perez@etud.univ-pau.fr>
2018-11-15 19:56:13 +0100

    empeche la commande si membre non inscrit

--- dashboard/includes/checkmemberjam.php
+++ dashboard/includes/checkmemberjam.php
@@ -4,12 +4,6 @@ $selectstatus = $db->query("SELECT status FROM users WHERE id='$user_id'");
 $s = $selectstatus->fetch(PDO::FETCH_OBJ);
 $status = $s->status;
 if($status != "MEMBRE"){
-  $access = 0;
-}else{
-  $access = 1;
+  header('Location: https://dashboard.jam-mdm.fr/downandupfichiers.php');
 }
-
-
-
-
  ?>
