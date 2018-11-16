d7fe3f6649fa81b91874dd77a81319fa381fb8f9
Paul Boussard <boussard.paul@hotmail.com>
2018-11-16 21:50:53 +0100

    Update activitees.php

--- dashboard/activitees.php
+++ dashboard/activitees.php
@@ -35,7 +35,7 @@ require_once('includes/head.php');
 
                               <h2 class="card-title text-center"><?php echo $s->title; ?></h2>
                               <div class="row">
-                                  <div class="col-md-4">
+                                  <div class="col-md-4  col-md-offset-1">
                                       <div class="card-content">
                                           <div class="info info-horizontal">
                                               <div class="icon icon-rose">
