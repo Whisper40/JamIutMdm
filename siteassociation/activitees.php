c3649fce1cc745625de52c79729bc45e48f92e5b
Paul Boussard <boussard.paul@hotmail.com>
2018-11-16 18:29:18 +0100

    modif

--- siteassociation/activitees.php
+++ siteassociation/activitees.php
@@ -37,20 +37,25 @@
 
 <body class="landing-page sidebar-collapse">
   <div class="wrapper">
-    <div class="wrapper">
+
+<?php
+  require_once('includes/navbar.php');
+
+  $head = $db->query("SELECT * FROM photopage WHERE nompage = '$nompage'");
+  $pagehead = $head->fetch(PDO::FETCH_OBJ);
+?>
+
       <div class="page-header page-header-small">
-        <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/bg1.jpg');">
+        <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/<?php echo $pagehead->image; ?>');">
         </div>
         <div class="content-center">
           <div class="container">
-            <h1 class="title">Activitées et Voyages</h1>
+            <h1 class="title"><?php echo $pagehead->pagetitre; ?></h1>
           </div>
         </div>
       </div>
 
 <?php
-    require_once('includes/navbar.php');
-
 
   if(isset($_GET['showmethisactivity'])){
 		$product = htmlentities($_GET['showmethisactivity']);
@@ -272,8 +277,8 @@
         <div class="container">
           <div class="row">
             <div class="col-md-8 ml-auto mr-auto text-center">
-              <h2 class="title">Who we are?</h2>
-              <h5 class="description">According to the National Oceanic and Atmospheric Administration, Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice extent in the Pacific and a late drop in ice extent in the Barents Sea.</h5>
+              <h2 class="title"><?php echo $pagehead->titre; ?></h2>
+              <h5 class="description"><?php echo $pagehead->description; ?></h5>
             </div>
           </div>
         </div>
--- siteassociation/actualitees.php
+++ siteassociation/actualitees.php
@@ -35,18 +35,20 @@
 }
 </style>
 
+<body class="landing-page sidebar-collapse">
+    <div class="wrapper">
+
 <?php
     require_once('includes/navbar.php');
 
-    ?>
+?>
 
-    <div class="wrapper">
       <div class="page-header page-header-small">
-        <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/bg1.jpg');">
+        <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/<?php echo $pagehead->image; ?>');">
         </div>
         <div class="content-center">
           <div class="container">
-            <h1 class="title">L'actualité</h1>
+            <h1 class="title"><?php echo $pagehead->pagetitre; ?></h1>
           </div>
         </div>
       </div>
@@ -239,8 +241,8 @@
       <div class="container">
         <div class="row">
           <div class="col-md-8 ml-auto mr-auto text-center">
-            <h2 class="title">Who we are?</h2>
-            <h5 class="description">According to the National Oceanic and Atmospheric Administration, Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice extent in the Pacific and a late drop in ice extent in the Barents Sea.</h5>
+            <h2 class="title"><?php echo $pagehead->titre; ?></h2>
+            <h5 class="description"><?php echo $pagehead->description; ?></h5>
           </div>
         </div>
       </div>
