fcb11e6eeacf6ecdf1415e8d9d0fbfa2868e8eca
Paul Boussard <boussard.paul@hotmail.com>
2018-11-17 14:55:18 +0100

    Update activitees.php

--- dashboard/activitees.php
+++ dashboard/activitees.php
@@ -211,7 +211,7 @@ require_once('includes/head.php');
 
                                                       <center>
                                                       <h4 class="info-title">Prix Total : <?php echo $total;?>€</h4>
-                                                      <br><br>
+                                                      <br>
                                                         <?php
                                                       if($stock>0){
                                                          ?>
