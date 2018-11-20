192674e6e36d52a65f4dff5cf96d3e2de0c7dff7
Paul Boussard <boussard.paul@hotmail.com>
2018-11-20 19:36:47 +0100

    Update activitees.php

--- dashboard/activitees.php
+++ dashboard/activitees.php
@@ -4,7 +4,6 @@ require_once('includes/connectBDD.php');
 require_once('includes/checkconnection.php');
 $nompage = "Les activitées en cours";
 require_once('includes/head.php');
-require_once('includes/checkmemberjam.php');
 
 ?>
 <script src="https://www.paypalobjects.com/api/checkout.js"></script>
