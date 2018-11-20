766a76c2b36f10517ef0782e770f4dfa6c1674d1
Paul Boussard <boussard.paul@hotmail.com>
2018-11-20 22:23:03 +0100

    Update devenirmembre.php

--- dashboard/devenirmembre.php
+++ dashboard/devenirmembre.php
@@ -67,9 +67,16 @@ require_once('includes/head.php');
                                           </div>
                                           <div class="description">
                                               <h4 class="info-title">Marketing</h4>
-                                              <p class="description">
-                                                  We've created the marketing campaign of the website. It was a very interesting collaboration.
-                                              </p>
+                                              <div class="content">
+                                                    <div class="container-fluid">
+                                                        <div class="row">
+                                                        <h3 class="title text-center">Mes documents</h3>
+                                                        <center><a href="./download.php?dl=thisfilecontainsyourdocumentsforjam" class="btn btn-rose btn-round">
+                                                                                                        <i class="material-icons">vertical_align_bottom</i> Télécharger les documents
+                                                                                                    </a></center>
+                                                                                                  </div>
+                                                                                                </div>
+                                                                                              </div>
                                           </div>
                                       </div>
                                       <div class="info info-horizontal">
@@ -80,17 +87,6 @@ require_once('includes/head.php');
                                               <h4 class="info-title">Fully Coded in HTML5</h4>
                                               <p class="description">
                                                   We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub.
-                                              </p>
-                                          </div>
-                                      </div>
-                                      <div class="info info-horizontal">
-                                          <div class="icon icon-info">
-                                              <i class="material-icons">group</i>
-                                          </div>
-                                          <div class="description">
-                                              <h4 class="info-title">Built Audience</h4>
-                                              <p class="description">
-                                                  There is also a Fully Customizable CMS Admin Dashboard for this product.
                                               </p>
                                           </div>
                                       </div>
