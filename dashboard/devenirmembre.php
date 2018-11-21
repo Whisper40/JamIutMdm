410796dfb37f28803e238d5dc4cd22675d883f4b
Paul Boussard <boussard.paul@hotmail.com>
2018-11-21 17:06:39 +0100

    Update devenirmembre.php

--- dashboard/devenirmembre.php
+++ dashboard/devenirmembre.php
@@ -131,60 +131,136 @@ $status = $s->status;
                                       <div class="table-responsive">
                                           <table class="table">
                                               <thead class="text-primary">
-                                                  <th>Name</th>
-                                                  <th>Country</th>
-                                                  <th>City</th>
-                                                  <th>Salary</th>
+                                                  <th>Nom</th>
+                                                  <th>Date d'envoie</th>
+                                                  <th>Status</th>
                                               </thead>
+
+                                              <?php
+                                              $user_id = $_SESSION['user_id'];
+                                              $sql = "SELECT * FROM validationfichiers WHERE user_id='$user_id' ORDER BY date ASC";
+                                              $req = $db->query($sql);
+                                              $req->setFetchMode(PDO::FETCH_ASSOC);
+
+                                              foreach($req as $row){
+                                              ?>
+
                                               <tbody>
                                                   <tr>
-                                                      <td>Dakota Rice</td>
-                                                      <td>Niger</td>
-                                                      <td>Oud-Turnhout</td>
-                                                      <td class="text-primary">$36,738</td>
+                                                      <td><?php echo $row['filename']; ?></td>
+                                                      <td><?php echo $row['date']; ?></td>
+                                                      <td><?php echo $row['status']; ?></td>
                                                   </tr>
-                                                  <tr>
-                                                      <td>Minerva Hooper</td>
-                                                      <td>Curaçao</td>
-                                                      <td>Sinaai-Waas</td>
-                                                      <td class="text-primary">$23,789</td>
-                                                  </tr>
-                                                  <tr>
-                                                      <td>Sage Rodriguez</td>
-                                                      <td>Netherlands</td>
-                                                      <td>Baileux</td>
-                                                      <td class="text-primary">$56,142</td>
-                                                  </tr>
-                                                  <tr>
-                                                      <td>Philip Chaney</td>
-                                                      <td>Korea, South</td>
-                                                      <td>Overland Park</td>
-                                                      <td class="text-primary">$38,735</td>
-                                                  </tr>
-                                                  <tr>
-                                                      <td>Doris Greene</td>
-                                                      <td>Malawi</td>
-                                                      <td>Feldkirchen in Kärnten</td>
-                                                      <td class="text-primary">$63,542</td>
-                                                  </tr>
-                                                  <tr>
-                                                      <td>Mason Porter</td>
-                                                      <td>Chile</td>
-                                                      <td>Gloucester</td>
-                                                      <td class="text-primary">$78,615</td>
-                                                  </tr>
+
+                                                  <?php } ?>
+
                                               </tbody>
                                           </table>
                                       </div>
-                                </div>
-                            </div>
-
+                                  </div>
+                              </div>
                           </div>
                       </div>
                   </div>
               </div>
           </div>
       </div>
+
+      <?php
+      if(isset($_POST['submit'])){
+        if(isset($_POST['message'])){
+          $message = $_POST['message'];
+        }else{
+          $message = "Aucun message";
+        }
+          $responseData = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']));
+          if($responseData->success){
+            $user_id = $_SESSION['user_id'];
+            $target_dir = "../../../JamFichiers/Uploads";
+
+            if (file_exists($target_dir/$user_id)) {
+              $target_dirnew = "$target_dir/$user_id/";
+            }else{
+              mkdir("$target_dir/$user_id", 0700);
+              $target_dirnew = "$target_dir/$user_id/";
+            }
+
+      $total = count($_FILES['fileToUpload']['name']);
+      for( $i=0 ; $i < $total ; $i++ ) {
+      $target_file = $target_dirnew . basename($_FILES["fileToUpload"]["name"][$i]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      // Check if file already exists
+      if (file_exists($target_file)) {
+          echo "Sorry, file already exists.";
+          $uploadOk = 0;
+      }
+      // Check file size < 2mo
+      if ($_FILES["fileToUpload"]["size"][$i] > 2000000) {
+          echo "Sorry, your file is too large.";
+          $uploadOk = 0;
+      }
+      // Allow certain file formats
+      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      && $imageFileType != "gif" && $imageFileType != "pdf" && $imageFileType != "zip" && $imageFileType != "rar") {
+          echo "Sorry, only JPG, JPEG, PNG & GIF, ZIP and RAR files are allowed.";
+          $uploadOk = 0;
+      }
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+          echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+      } else {
+        date_default_timezone_set('Europe/Paris');
+        setlocale(LC_TIME, 'fr_FR.utf8','fra');
+        $date = strftime('%d:%m:%y %H:%M:%S');
+
+        $target_filefile = basename($_FILES["fileToUpload"]["name"][$i]);
+        $target_file2 = $target_dirnew."".$date.basename($_FILES["fileToUpload"]["name"][$i]);
+          if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file2)) {
+              echo "The file ". basename( $_FILES["fileToUpload"]["name"][$i]). " has been uploaded.";
+              $status = "EN ATTENTE DE VALIDATION";
+              date_default_timezone_set('Europe/Paris');
+              setlocale(LC_TIME, 'fr_FR.utf8','fra');
+              $date = strftime('%d/%m/%y %H:%M:%S');
+              $insertinfos = $db->prepare("INSERT INTO validationfichiers (user_id, filename, message, ip, date, status) VALUES(:user_id, :filename, :message, :ip, :date, :status)");
+              $insertinfos->execute(array(
+
+                  "user_id"=>$_SESSION['user_id'],
+                  "filename"=>$target_filefile,
+                  "message"=>$message,
+                  "ip"=>$ip,
+                  "date"=>$date,
+                  "status"=>$status
+                  )
+              );
+
+          }else {
+              echo "Sorry, there was an error uploading your file.";
+          } } }
+          }else{
+          ?>
+
+            <div class="content">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-3">
+
+
+
+            <div class="alert alert-danger">
+                    <div class="container">
+                <div class="alert-icon">
+                  <i class="material-icons">info_outline</i>
+                </div>
+
+
+                      <b>Erreur Captcha:</b> BipBoup BoupBip ! Robot détecté !
+                    </div>
+                </div>
+              </div></div></div></div>
+            <?php  }  } ?>
+
   </body>
 
 <?php
