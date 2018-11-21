267c9c8d965f25478d71ee82d696226b5b203b04
Paul Boussard <boussard.paul@hotmail.com>
2018-11-21 15:44:31 +0100

    Update devenirmembre.php

--- dashboard/devenirmembre.php
+++ dashboard/devenirmembre.php
@@ -36,44 +36,10 @@ require_once('includes/head.php');
                                               <i class="material-icons">timeline</i>
                                           </div>
                                           <div class="description">
-                                              <h4 class="info-title">Marketing</h4>
+                                              <h4 class="info-title">Etape 1</h4>
                                               <p class="description">
                                                   We've created the marketing campaign of the website. It was a very interesting collaboration.
                                               </p>
-                                          </div>
-                                      </div>
-                                      <div class="info info-horizontal">
-                                          <div class="icon icon-primary">
-                                              <i class="material-icons">code</i>
-                                          </div>
-                                          <div class="description">
-                                              <h4 class="info-title">Fully Coded in HTML5</h4>
-                                              <p class="description">
-                                                  We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub.
-                                              </p>
-                                          </div>
-                                      </div>
-                                      <div class="info info-horizontal">
-                                          <div class="icon icon-info">
-                                              <i class="material-icons">group</i>
-                                          </div>
-                                          <div class="description">
-                                              <h4 class="info-title">Built Audience</h4>
-                                              <p class="description">
-                                                  There is also a Fully Customizable CMS Admin Dashboard for this product.
-                                              </p>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="col-md-6">
-                                  <div class="card-content">
-                                      <div class="info info-horizontal">
-                                          <div class="icon icon-rose">
-                                              <i class="material-icons">timeline</i>
-                                          </div>
-                                          <div class="description">
-                                              <h4 class="info-title">Marketing</h4>
                                               <div class="content">
                                                     <div class="container-fluid">
                                                         <div class="row">
@@ -86,6 +52,11 @@ require_once('includes/head.php');
                                                                                               </div>
                                           </div>
                                       </div>
+
+                                  </div>
+                              </div>
+                              <div class="col-md-6">
+                                  <div class="card-content">
                                       <div class="info info-horizontal">
                                           <div class="icon icon-primary">
                                               <i class="material-icons">code</i>
