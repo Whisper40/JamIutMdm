07645af35f8571afc09af67dab9c3b68d81667da
Paul Boussard <boussard.paul@hotmail.com>
2018-11-21 15:53:17 +0100

    Update devenirmembre.php

--- dashboard/devenirmembre.php
+++ dashboard/devenirmembre.php
@@ -60,6 +60,65 @@ require_once('includes/head.php');
                                               <p class="description">
                                                   We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub.
                                               </p>
+                                              <div class="content">
+                                                  <div class="container-fluid">
+                                                      <div class="row">
+                                                          <center>
+                                                            <div class="card">
+                                                                <form  method="POST" class="form-horizontal"  enctype="multipart/form-data">
+                                                                    <div class="card-header card-header-text" data-background-color="rose">
+                                                                        <h4 class="card-title">Nous contacter</h4>
+                                                                    </div>
+                                                                    <div class="card-content">
+
+
+                                                    <div class="row">
+                                                        <label class="col-sm-2 label-on-left">Message : </label>
+                                                        <div class="col-sm-10">
+                                                            <div class="form-group label-floating is-empty">
+                                                                <textarea name="message" class="form-control" rows="6">
+                                                            </textarea>
+                                                        <span class="help-block">Merci de décrire précisément votre message</span></div>
+                                                        </div>
+                                                    </div>
+
+                                                     <div class="row">
+                                                        <label class="col-sm-2 label-on-left"> </label>
+                                                        <div class="col-sm-10">
+                                                            <div class="form-group label-floating is-empty">
+
+
+                                                                <div class="form-group form-file-upload">
+                          <input type="file" id="fileToUpload" name="fileToUpload[]" multiple="multiple">
+                          <div class="input-group">
+                            <input type="text" readonly="" class="form-control" placeholder="Insérer votre pièce jointe">
+                            <span class="input-group-btn input-group-s">
+                              <button type="button" class="btn btn-just-icon btn-round btn-info">
+                                <i class="material-icons">layers</i>
+                              </button>
+                            </span>
+                          </div>
+                        </div>
+                                                      </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <center>
+                                                    <div class="g-recaptcha" data-sitekey="<?= $sitekey; ?>"></div>
+                                                  </center>
+                                                </div>
+                                                <center>
+                                                  <input type="submit" name="submit" value="Envoyer un message" class="btn btn-primary btn-round" />
+
+          </center>
+
+
+                                                                </form>
+
+                                                          </center>
+                                                      </div>
+                                                  </div>
+                                              </div>
                                           </div>
                                       </div>
                                   </div>
