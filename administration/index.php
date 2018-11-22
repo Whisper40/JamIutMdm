75e9e7d028871640472a17be5ffea50b2af9fb85
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-11-22 14:41:09 +0100

    Update index.php

--- administration/index.php
+++ administration/index.php
@@ -362,6 +362,7 @@ $valyear0 = date("Y");
     $month5 = $db->query("SELECT SUM(amount) AS totalamount5 FROM transactions WHERE MONTH(datesystem) = '$valmonth5' and YEAR(datesystem) = '$valyear5'");
     $r = $month5->fetch(PDO::FETCH_OBJ);
     $countmonth5 = $r->totalamount5;
+
     ?>
     <h1> <?php echo $countmonth5; ?></h1><?php
 
