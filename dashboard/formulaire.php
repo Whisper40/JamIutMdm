d52ef401a8c85c80bd7535c7dd0aae4bec467833
Paul Boussard <boussard.paul@hotmail.com>
2018-11-26 20:51:32 +0100

    Modif

--- dashboard/formulaire.php
+++ dashboard/formulaire.php
@@ -81,56 +81,63 @@ if ($type == 'ski'){
 
            ?>
 
-
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header card-header-icon" data-background-color="rose">
-                        <i class="material-icons">mail_outline</i>
-                    </div>
-
-                    <div class="card-content">
-                        <h4 class="card-title">Modifier mes informations</h4>
-                        <form action="" method="post" id="myForm1" class="contact-form">
-                            <div class="form-group label-floating">
-                                <label class="control-label">Poids (Kg)</label>
-
-                                <input type="number" class="form-control" value="<?php echo $poids; ?>"name="poids" id="poids">
-
-                            </div>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Taille (cm)</label>
-                                <input type="number" name="taille" value="<?php echo $taille; ?>"id="taille" class="form-control">
-                            </div>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Allèrgies</label>
-                                <input type="text" name="allergie" value="<?php echo $allergie; ?>"id="allergie" class="form-control">
-                            </div>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Adresse</label>
-                                <input type="text" name="adresse" value="<?php echo $adresse; ?>"id="adresse" class="form-control">
-                            </div>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Code Postal</label>
-                                <input type="number" name="codepostal" value="<?php echo $codepostal; ?>" id="codepostal" class="form-control">
-                            </div>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Ville</label>
-                                <input type="text" name="ville" value="<?php echo $ville; ?>" id="ville" class="form-control">
-                            </div>
-                            <div class="form-group label-floating">
-                                <label class="control-label">Téléphone d'urgence</label>
-                                <input type="number" name="telurgence" value="<?php echo $telurgence; ?>" id="telurgence" class="form-control">
-                            </div>
-                            <center>
-                            <button id="submitFormDataSki" onclick="SubmitFormDataSki();" type="button" class="btn btn-fill btn-rose">Modifier</button>
-                            </center>
-                        </form>
-                    </div>
-                  </div>
-
-
-                </div>
-
+           <div class="content">
+               <div class="container-fluid">
+                   <div class="card">
+                       <div class="card-content">
+                           <h2 class="card-title text-center"><?php echo $s->title; ?></h2>
+                           <form action="" method="post" id="myForm1" class="contact-form">
+                           <div class="row">
+                               <div class="col-sm-6">
+                                   <div class="card-content">
+                                     <div class="form-group label-floating">
+                                         <label class="control-label">Poids (Kg)</label>
+                                         <input type="number" class="form-control" value="<?php echo $poids; ?>"name="poids" id="poids">
+                                     </div>
+                                     <div class="form-group label-floating">
+                                         <label class="control-label">Taille (cm)</label>
+                                         <input type="number" name="taille" value="<?php echo $taille; ?>"id="taille" class="form-control">
+                                     </div>
+                                     <div class="form-group label-floating">
+                                         <label class="control-label">Allèrgies</label>
+                                         <input type="text" name="allergie" value="<?php echo $allergie; ?>"id="allergie" class="form-control">
+                                     </div>
+                                    </div>
+                                 </div>
+                                 <div class="col-sm-6">
+                                     <div class="card-content">
+                                       <div class="form-group label-floating">
+                                           <label class="control-label">Adresse</label>
+                                           <input type="text" name="adresse" value="<?php echo $adresse; ?>"id="adresse" class="form-control">
+                                       </div>
+                                       <div class="form-group label-floating">
+                                           <label class="control-label">Code Postal</label>
+                                           <input type="number" name="codepostal" value="<?php echo $codepostal; ?>" id="codepostal" class="form-control">
+                                       </div>
+                                       <div class="form-group label-floating">
+                                           <label class="control-label">Ville</label>
+                                           <input type="text" name="ville" value="<?php echo $ville; ?>" id="ville" class="form-control">
+                                       </div>
+                                    </div>
+                               </div>
+                           </div>
+                           <div class="row">
+                               <div class="col-sm-12">
+                                   <div class="card-content">
+                                     <div class="form-group label-floating">
+                                         <label class="control-label">Téléphone d'urgence</label>
+                                         <input type="number" name="telurgence" value="<?php echo $telurgence; ?>" id="telurgence" class="form-control">
+                                     </div>
+                                     <center>
+                                     <button id="submitFormDataSki" onclick="SubmitFormDataSki();" type="button" class="btn btn-fill btn-rose">Modifier</button>
+                                     </center>
+                                    </div>
+                                 </div>
+                           </div>
+                         </form>
+                       </div>
+                   </div>
+               </div>
 
             <div id="results1"> <!-- TRES IMPORTANT -->
 
