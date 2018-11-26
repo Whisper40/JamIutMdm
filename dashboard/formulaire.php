a8d2d07cc330e790adbe1314a9daa4b374fd187f
Paul Boussard <boussard.paul@hotmail.com>
2018-11-26 21:26:25 +0100

    Update formulaire.php

--- dashboard/formulaire.php
+++ dashboard/formulaire.php
@@ -473,25 +473,6 @@ if(!empty($infoscomplementaires3)){
 
 
 
-
-
-
-
-             <div class="container-fluid">
-                 <div class="row">
-                     <div class="col-md-6">
-
-                                  </div>
-
-
-<div class="col-md-6">
-
-</div>
-</div>
-</div>
-
-
-
 <?php
 
 if(!empty($_POST['jeneparticipeplus'])){
