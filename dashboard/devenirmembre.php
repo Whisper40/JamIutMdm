83f7e9a47ee79776b5aaf0666226c2fb7c2453d7
Unknown <perez40390@hotmail.fr>
2018-11-27 21:23:32 +0100

    test

--- dashboard/devenirmembre.php
+++ dashboard/devenirmembre.php
@@ -272,27 +272,6 @@ client: {
 
           <!-- RAJOUT KEVIN -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
           <!-- FIN RAJOUT KEVIN -->
 
       <?php
@@ -366,7 +345,25 @@ client: {
               );
 
           }else {
-              echo "Sorry, there was an error uploading your file.";
+              ?>
+               <div class="container">
+           <div class="row">
+             <div class="col-sm-18 ml-auto mr-auto">
+              <div class="alert alert-danger">
+                 <div class="alert-icon">
+                   <i class="now-ui-icons ui-1_bell-53"></i>
+                 </div>
+                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                   <span aria-hidden="true"><i class="now-ui-icons ui-1_simple-remove"></i></span>
+                 </button>
+                    <b>Erreur :</b> Fichiers non uploadés !
+              </div>
+            </div>
+           </div>
+        </div>
+
+
+              <?php
           } } }
           }else{
           ?>
