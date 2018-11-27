d6f4497414cd545b74e95586c7df2c8a40dca213
Paul Boussard <boussard.paul@hotmail.com>
2018-11-27 20:51:37 +0100

    Update don.php

--- siteassociation/don.php
+++ siteassociation/don.php
@@ -203,13 +203,17 @@ if(!empty($_POST['optionsRadios'])){ // SI LE MONTANT EST SAISIT
     <div class="container">
       <h2 class="title"><?php echo $pagehead->titre; ?></h2>
       <p class="description"><?php echo $pagehead->description; ?></p>
-    </div>
-  </div><br><br>
+      <div class="row">
+        <div class="col-lg-6 text-center col-md-8 ml-auto mr-auto">
           <div align="center" id="paypal-button"></div>
 
           <div class="send-button">
             <a  href="https://jam-mdm.fr/don.php" class="btn btn-primary btn-round btn-block" name="submit">Annuler le Paiement</a>
           </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 <?php
