6d20d3330ef741157021c6e3aec34b6c496d247d
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-11-28 20:57:04 +0100

    Update devenirmembre.php

--- dashboard/devenirmembre.php
+++ dashboard/devenirmembre.php
@@ -19,6 +19,103 @@ $s = $selectstatus->fetch(PDO::FETCH_OBJ);
 $status = $s->status;?>
 
 
+
+
+
+      <?php
+      if(isset($_POST['submit'])){
+        if(isset($_POST['message'])){
+          $message = $_POST['message'];
+        }else{
+          $message = "Aucun message";
+        }
+          $responseData = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']));
+          if($responseData->success){
+            $user_id = $_SESSION['user_id'];
+            $target_dir = "../../../JamFichiers/Uploads";
+            if (file_exists($target_dir/$user_id)) {
+              $target_dirnew = "$target_dir/$user_id/";
+            }else{
+              mkdir("$target_dir/$user_id", 0700);
+              $target_dirnew = "$target_dir/$user_id/";
+            }
+      $total = count($_FILES['fileToUpload']['name']);
+      for( $i=0 ; $i < $total ; $i++ ) {
+      $target_file = $target_dirnew . basename($_FILES["fileToUpload"]["name"][$i]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      // Check if file already exists
+      if (file_exists($target_file)) {
+          echo "Sorry, file already exists.";
+          $uploadOk = 0;
+      }
+      // Check file size < 2mo
+      if ($_FILES["fileToUpload"]["size"][$i] > 2000000) {
+          echo "Sorry, your file is too large.";
+          $uploadOk = 0;
+      }
+      // Allow certain file formats
+      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+      && $imageFileType != "gif" && $imageFileType != "pdf" && $imageFileType != "zip" && $imageFileType != "rar") {
+          echo "Sorry, only JPG, JPEG, PNG & GIF, ZIP and RAR files are allowed.";
+          $uploadOk = 0;
+      }
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+          echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+      } else {
+        date_default_timezone_set('Europe/Paris');
+        setlocale(LC_TIME, 'fr_FR.utf8','fra');
+        $date = strftime('%d:%m:%y %H:%M:%S');
+        $target_filefile = basename($_FILES["fileToUpload"]["name"][$i]);
+        $target_file2 = $target_dirnew."".$date.basename($_FILES["fileToUpload"]["name"][$i]);
+          if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file2)) {
+              echo "The file ". basename( $_FILES["fileToUpload"]["name"][$i]). " has been uploaded.";
+              $status = "EN ATTENTE DE VALIDATION";
+              date_default_timezone_set('Europe/Paris');
+              setlocale(LC_TIME, 'fr_FR.utf8','fra');
+              $date = strftime('%d/%m/%y %H:%M:%S');
+              $insertinfos = $db->prepare("INSERT INTO validationfichiers (user_id, filename,filenamesystem, message, ip, date, status) VALUES(:user_id, :filename, :filenamesystem, :message, :ip, :date, :status)");
+              $insertinfos->execute(array(
+                  "user_id"=>$_SESSION['user_id'],
+                  "filename"=>$target_filefile,
+                  "filenamesystem"=>$target_file2,
+                  "message"=>$message,
+                  "ip"=>$ip,
+                  "date"=>$date,
+                  "status"=>$status
+                  )
+              );
+          }else {
+              ?>
+               <div class="container">
+           <div class="row">
+             <div class="col-sm-18 ml-auto mr-auto">
+              <div class="alert alert-danger">
+                 <div class="alert-icon">
+                   <i class="now-ui-icons ui-1_bell-53"></i>
+                 </div>
+                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                   <span aria-hidden="true"><i class="now-ui-icons ui-1_simple-remove"></i></span>
+                 </button>
+                    <b>Erreur :</b> Fichiers non uploadés !
+              </div>
+            </div>
+           </div>
+        </div>
+
+
+              <?php
+          } } }
+          }else{
+echo 'james';
+
+
+          }  } ?>
+          
+
+
 <body>
     <div class="wrapper">
 
@@ -257,97 +354,6 @@ client: {
 
           <!-- FIN RAJOUT KEVIN -->
 
-      <?php
-      if(isset($_POST['submit'])){
-        if(isset($_POST['message'])){
-          $message = $_POST['message'];
-        }else{
-          $message = "Aucun message";
-        }
-          $responseData = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']));
-          if($responseData->success){
-            $user_id = $_SESSION['user_id'];
-            $target_dir = "../../../JamFichiers/Uploads";
-            if (file_exists($target_dir/$user_id)) {
-              $target_dirnew = "$target_dir/$user_id/";
-            }else{
-              mkdir("$target_dir/$user_id", 0700);
-              $target_dirnew = "$target_dir/$user_id/";
-            }
-      $total = count($_FILES['fileToUpload']['name']);
-      for( $i=0 ; $i < $total ; $i++ ) {
-      $target_file = $target_dirnew . basename($_FILES["fileToUpload"]["name"][$i]);
-      $uploadOk = 1;
-      $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-      // Check if file already exists
-      if (file_exists($target_file)) {
-          echo "Sorry, file already exists.";
-          $uploadOk = 0;
-      }
-      // Check file size < 2mo
-      if ($_FILES["fileToUpload"]["size"][$i] > 2000000) {
-          echo "Sorry, your file is too large.";
-          $uploadOk = 0;
-      }
-      // Allow certain file formats
-      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-      && $imageFileType != "gif" && $imageFileType != "pdf" && $imageFileType != "zip" && $imageFileType != "rar") {
-          echo "Sorry, only JPG, JPEG, PNG & GIF, ZIP and RAR files are allowed.";
-          $uploadOk = 0;
-      }
-      // Check if $uploadOk is set to 0 by an error
-      if ($uploadOk == 0) {
-          echo "Sorry, your file was not uploaded.";
-      // if everything is ok, try to upload file
-      } else {
-        date_default_timezone_set('Europe/Paris');
-        setlocale(LC_TIME, 'fr_FR.utf8','fra');
-        $date = strftime('%d:%m:%y %H:%M:%S');
-        $target_filefile = basename($_FILES["fileToUpload"]["name"][$i]);
-        $target_file2 = $target_dirnew."".$date.basename($_FILES["fileToUpload"]["name"][$i]);
-          if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file2)) {
-              echo "The file ". basename( $_FILES["fileToUpload"]["name"][$i]). " has been uploaded.";
-              $status = "EN ATTENTE DE VALIDATION";
-              date_default_timezone_set('Europe/Paris');
-              setlocale(LC_TIME, 'fr_FR.utf8','fra');
-              $date = strftime('%d/%m/%y %H:%M:%S');
-              $insertinfos = $db->prepare("INSERT INTO validationfichiers (user_id, filename,filenamesystem, message, ip, date, status) VALUES(:user_id, :filename, :filenamesystem, :message, :ip, :date, :status)");
-              $insertinfos->execute(array(
-                  "user_id"=>$_SESSION['user_id'],
-                  "filename"=>$target_filefile,
-                  "filenamesystem"=>$target_file2,
-                  "message"=>$message,
-                  "ip"=>$ip,
-                  "date"=>$date,
-                  "status"=>$status
-                  )
-              );
-          }else {
-              ?>
-               <div class="container">
-           <div class="row">
-             <div class="col-sm-18 ml-auto mr-auto">
-              <div class="alert alert-danger">
-                 <div class="alert-icon">
-                   <i class="now-ui-icons ui-1_bell-53"></i>
-                 </div>
-                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                   <span aria-hidden="true"><i class="now-ui-icons ui-1_simple-remove"></i></span>
-                 </button>
-                    <b>Erreur :</b> Fichiers non uploadés !
-              </div>
-            </div>
-           </div>
-        </div>
-
-
-              <?php
-          } } }
-          }else{
-echo 'james';
-
-
-          }  } ?>
 
       </div>
   </body>
