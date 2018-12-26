f3c139e8db78c62898133e9044b51891c28c2d56
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-12-26 18:56:12 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -382,7 +382,7 @@ if($_GET['page']=='index'){
 
 
 
-  $selectid = $db->prepare("SELECT * FROM membres");
+  $selectid = $db->prepare("SELECT distinct ID FROM membres");
   $selectid->execute();
   $countid = $selectid->rowCount();
 
