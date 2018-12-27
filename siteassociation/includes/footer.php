7114b100906709a8eeaa4f4ad1404db89859ba9e
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-12-27 18:16:21 +0100

    Update footer.php

--- siteassociation/includes/footer.php
+++ siteassociation/includes/footer.php
@@ -13,3 +13,27 @@
 
 </div>
 </body>
+<!-- Quantcast Tag -->
+<script type="text/javascript">
+var _qevents = _qevents || [];
+
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
+qacct:"p-HVdbXR0aGEp8j"
+});
+</script>
+
+<noscript>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-HVdbXR0aGEp8j.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
+</noscript>
+<!-- End Quantcast tag -->
