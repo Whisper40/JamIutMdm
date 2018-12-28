3f84d5a19acde5a5b6974b705679b5d5a7d6b6a9
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-12-28 12:03:11 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -716,6 +716,13 @@ if ($categorie == 'tres'){
 </div>
 
 <?php
+if (isset($error)){
+  echo $error;
+}else if (isset($succes)){
+  echo $succes;
+}else{
+  echo 'rien';
+}
 
 if(isset($_POST['submitphotomembre'])){
 
