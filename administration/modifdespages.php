1fb3f8f247f8ba3fde727d793704f087b659421c
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-12-30 17:43:01 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -1904,20 +1904,174 @@ $selectcatimages=$db->query("SELECT DISTINCT title FROM newsactus");
             <input type="submit" name="submitphotoactualite" value="Envoyer les images !">
         </form>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 </div>
+
+
+
+
+<script>
+$(document).ready(function(){
+
+var $recherche =$('input[name=valeur]');
+var critere;
+$recherche.keyup(function(){
+  critere = $.trim($recherche.val());
+  if(critere!=''){
+    $.get('gestionrechercheimgactualitee.php?critere='+critere,function(retour){
+
+$('#resultat').html(retour).fadeIn();
+
+});
+
+}else $('#resultat').empty().fadeOut();
+});
+});
+</script>
+
+
+<?php
+
+if($_GET['action']=='unban'){
+
+$id=$_GET['id'];
+$setunban = $db->prepare("UPDATE images SET status='1' WHERE id=$id");
+$setunban->execute();
+?>
+<script>window.location="https://administration.jam-mdm.fr/gestionimage.php"</script>
+<?php
+}else if($_GET['action']=='ban'){
+
+$id=$_GET['id'];
+$setban = $db->prepare("UPDATE images SET status='0' WHERE id=$id");
+$setban->execute();
+?>
+<script>window.location="https://administration.jam-mdm.fr/gestionimage.php"</script>
+<?php
+}else if($_GET['action']=='delete'){
+
+$id=$_GET['id'];
+$selectnom = $db->query("SELECT * FROM images WHERE id='$id'");
+$rname = $selectnom->fetch(PDO::FETCH_OBJ);
+$valnom = $rname->file_name;
+$dossier = $rname->title;
+
+$target_dir = '../../../JamFichiers/Photos';
+$original = 'Original';
+$affiche = 'Affiche';
+$thumb = 'Thumb';
+echo 'Jamesbond';
+echo "$target_dir.'/'.$original.'/'.$dossier";
+
+if (file_exists($target_dir.'/'.$original.'/'.$dossier)){
+  echo 'Jesuisrnetré';
+  unlink("$target_dir/$original/$dossier/$valnom");
+  unlink("$target_dir/$affiche/$dossier/$valnom");
+  unlink("$target_dir/$thumb/$dossier/$valnom");
+  echo 'deleted';
+  $updatedelete = $db->prepare("DELETE FROM images WHERE id=$id");
+  $updatedelete->execute();
+
+}else{
+  echo 'n extse pas';
+  $error = 'Un problème de répertoire est présent, contacter votre administrateur !';
+}
+echo 'esquive';
+
+?>
+<script>window.location="https://administration.jam-mdm.fr/gestionimage.php"</script>
+<?php
+}
+
+    require_once('includes/navbar.php');
+
+    $head = $db->query("SELECT * FROM photopage WHERE nompage = '$nompage'");
+    $pagehead = $head->fetch(PDO::FETCH_OBJ);
+?>
+
+    <div class="page-header page-header-small">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/<?php echo $pagehead->image; ?>');">
+      </div>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title"><?php echo $pagehead->pagetitre; ?></h1>
+        </div>
+      </div>
+    </div>
+
+    <?php
+
+    $selectbanimg = $db->prepare("SELECT * FROM images WHERE status='0'");
+    $selectbanimg->execute();
+    $countbanimg = $selectbanimg->rowCount();
+    if($countbanimg>'0'){
+    ?>
+    <h3> Images inactives </h3>
+    <table class="table">
+<thead>
+  <tr>
+    <th scope="col">Id</th>
+    <th scope="col">Nom</th>
+    <th scope="col">Catégorie</th>
+    <th scope="col">Album Actif</th>
+    <th scope="col">Action</th>
+  </tr>
+</thead>
+<tbody>
+<?php
+
+
+    while($sban=$selectbanimg->fetch(PDO::FETCH_OBJ)){
+      $idimg = $sban->id;
+      $file_name = $sban->file_name;
+      $title = $sban->title;
+      $albumactif = $sban->albumactif;
+?>
+    <tr>
+      <th scope="row"><?php echo $idimg;?></th>
+      <td><?php echo $file_name;?><td>
+      <td><?php echo $title;?></td>
+      <td><?php echo $albumactif;?></td>
+      <td>
+<a href="?action=unban&amp;id=<?php echo $idimg;?>">
+  <button type="button" class="btn">Activer</button>
+</a>
+<a href="?action=delete&amp;id=<?php echo $idimg;?>">
+  <button type="button" class="btn">Supprimer</button>
+</a>
+
+      </td>
+    </tr>
+<?php
+    }
+     ?>
+   </tbody>
+ </table>
+
+ <?php
+}else{
+  ?>
+<h3> Aucune image n'est actuellement bannie ! Inchallah </h3>
+  <?php
+}
+ ?>
+    <div class="section section-contact-us text-center">
+      <div class="container">
+        <h2 class="title">AUTRE</h2>
+        <p class="description">AUTRE</p>
+        <div class="row">
+          <div class="col-lg-6 text-center col-md-8 ml-auto mr-auto">
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+<h3> Bannir une image :  </h3>
+  <input type='text' name="valeur" placeholder="Saisir son nom, id ou email">
+  <p id='resultat'></p>
+
+
+
 <?php
 //FIn Création
 }
