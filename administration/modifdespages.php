3c73820d42e5eb60f4d730b08a97fd65537f3581
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-12-31 10:06:58 +0100

    Modification du système

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -2428,43 +2428,201 @@ if (file_exists($target_dir)){
   ?>
 
 
-  <script>
 
 
-  function SubmitFormDataCreateUneActu() {
-   var user_id = "<?php echo $_SESSION['admin_id']; ?>";
-   var title = $("#title").val();
-   var description = $("#description").val();
-   var formatimg = $("#formatimg").val();
-   var stock = $("#stock").val();
-   var datesejour = $("#datesejour").val();
-   var price = $("#price").val();
 
-   $.post("ajax/createuneactivitevoyage.php", { user_id:user_id, title: title, description: description, formatimg: formatimg, stock: stock, datesejour: datesejour, price: price},
-   function(data) {
-    $('#results11').html(data);
 
-   });
+  <?php
+  if(isset($_POST['submitactivite'])){
+    function slugify($text){
+      $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+      $text = preg_replace('~[^-\w]+~', '', $text);
+
+      $text = trim($text, '-');
+
+      $text = preg_replace('~-+~', '-', $text);
+
+      $text = strtolower($text);
+
+      if (empty($text)) {
+        return 'n-a';
+      }
+
+        return $text;
+    }
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $formatimg = $_POST['formatimg'];
+    $stock = $_POST['stock'];
+    $datesejour = $_POST['datesejour'];
+    $price = $_POST['price'];
+    $slug = slugify($title);
+    if($price>'0'){
+      $payant = '1';
+    }else{
+      $payant = '0';
+    }
+
+
+        $target_dir = "../../../JamFichiers/Img/ImagesDuSite";
+
+        $original = 'Original';
+        if (file_exists($target_dir/$original)) {
+          $target_dirnew = "$target_dir/$original/";
+        }else{
+          mkdir("$target_dir/$original", 0700);
+          $target_dirnew = "$target_dir/$original/";
+        }
+
+        //Ajout thumb
+        $thumb = 'Thumb';
+        if (file_exists($target_dir/$thumb)) {
+          $target_dirnewthumb = "$target_dir/$thumb/";
+        }else{
+          mkdir("$target_dir/$thumb", 0700);
+          $target_dirnewthumb = "$target_dir/$thumb/";
+        }
+        //FIN
+
+
+  $total = count($_FILES['fileToUpload']['name']);
+
+  for( $i=0 ; $i < $total ; $i++ ) {
+  $target_file = $target_dirnew . basename($_FILES["fileToUpload"]["name"][$i]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  // Check if file already exists
+  if (file_exists($target_file)) {
+      $error = 'Désolé, le fichier existe déja.';
+      $uploadOk = 0;
+  }
+  // Check file size < 2mo
+  if ($_FILES["fileToUpload"]["size"][$i] > 3000000) {
+      $error = 'Désolé, le fichier est trop grand.';
+      $uploadOk = 0;
 
   }
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+      $error = 'Désolé, les formats autorisés sont JPG, PNG et JPEG.';
+      $uploadOk = 0;
+  }
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+      $error = 'Désolé, une erreur est survenue.';
+  // if everything is ok, try to upload file
+  } else {
+    date_default_timezone_set('Europe/Paris');
+    setlocale(LC_TIME, 'fr_FR.utf8','fra');
+    $date = strftime('%d:%m:%y %H:%M:%S');
 
-  </script>
+    $target_filefile = basename($_FILES["fileToUpload"]["name"][$i]);
+    $target_file2 = $target_dirnew."".$date.basename($_FILES["fileToUpload"]["name"][$i]);
+    $target_file3 = $target_dirnew."".$slug);
+
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file3)) {
+          $succes = "Le fichier ". basename( $_FILES["fileToUpload"]["name"][$i]). " à bien été uploadé.";
+
+
+          $insert = $db->prepare("INSERT INTO newsactus (title, slug, description, surname, date, formatimg, status, stock, datesejour, price, payant) VALUES(:title, :slug, :description, :surname, :date, :formatimg, :status, :stock, :datesejour, :price, :payant)");
+          $insert->execute(array(
+                              "title"=>$title,
+                              "slug"=>$slug,
+                              "description"=>$description,
+                              "surname"=>'Activités / Voyages',
+                              "date"=>$date,
+                              "formatimg"=>$formatimg,
+                              "status"=>'ACTIVE',
+                              "stock"=>$stock,
+                              "datesejour"=>$datesejour,
+                              "price"=>$price,
+                              "payant"=>$payant
+                              )
+                          );
+
+
+
+          $insertlogs = $db->prepare("INSERT INTO logs (user_id, type, action, page, date) VALUES(:user_id, :type, :action, :page, :date)");
+          $insertlogs->execute(array(
+                              "user_id"=>$user_id,
+                              "type"=>'Ajout',
+                              "action"=>'Ajout d\'une actualité',
+                              "page"=>'actualitees.php',
+                              "date"=>$date
+                              )
+                          );
+
+
+
+          $status = '1';
+          date_default_timezone_set('Europe/Paris');
+          setlocale(LC_TIME, 'fr_FR.utf8','fra');
+          $date = strftime('%Y-%m-%d %H:%M:%S');
+
+          $img_tmp = $target_dirnew.$target_filefile;
+          $fin = $target_dirnewthumb.$target_filefile;
+
+
+            //TAILLE EN PIXELS DE L'IMAGE REDIMENSIONNEE
+              $longueur = 300;
+              $largeur = 220;
+              //TAILLE DE L'IMAGE ACTUELLE
+              $taille = getimagesize($img_tmp);
+              //SI LE FICHIER EXISTE
+              if ($taille) {
+                  //SI JPG
+                  if ($taille['mime']=='image/jpeg' ) {
+                            //OUVERTURE DE L'IMAGE ORIGINALE
+                              $img_big = imagecreatefromjpeg($img_tmp);
+                              $img_new = imagecreate($longueur, $largeur);
+                            //CREATION DE LA MINIATURE
+                              $img_petite = imagecreatetruecolor($longueur, $largeur) or $img_petite = imagecreate($longueur, $largeur);
+                              //COPIE DE L'IMAGE REDIMENSIONNEE
+                              imagecopyresampled($img_petite,$img_big,0,0,0,0,$longueur,$largeur,$taille[0],$taille[1]);
+                              imagejpeg($img_petite,$fin);
+                  }
+                //SI PNG
+              else if ($taille['mime']=='image/png' ) {
+                              //OUVERTURE DE L'IMAGE ORIGINALE
+                              $img_big = imagecreatefrompng($img_tmp); // On ouvre l'image d'origine
+                              $img_new = imagecreate($longueur, $largeur);
+                              //CREATION DE LA MINIATURE
+                              $img_petite = imagecreatetruecolor($longueur, $largeur) OR $img_petite = imagecreate($longueur, $largeur);
+                              //COPIE DE L'IMAGE REDIMENSIONNEE
+                              imagecopyresampled($img_petite,$img_big,0,0,0,0,$longueur,$largeur,$taille[0],$taille[1]);
+                              imagepng($img_petite,$fin);
+                          }
+
+
+                  }
+
+
+      }else {
+          $error = 'Désolé, une erreur est survenue.';
+      } } }
+
+            } ?>
+
   <div class="content">
     <div class="container-fluid">
         <div class="card">
             <div class="card-content">
-                <h2 class="card-title text-center">Création d'actualitée</h2>
-                <form action="" method="post" id="myForm1" class="contact-form">
+                <h2 class="card-title text-center">Création d'activité/voyages</h2>
+            <form  method="POST" class="form-horizontal"  enctype="multipart/form-data">
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="card-content">
                           <div class="form-group label-floating">
                               <label class="control-label">Titre</label>
-                              <input type="text" class="form-control" value="Titre de l'actualité" name="title" id="title">
+                              <input type="text" class="form-control" value="Titre de l'activité" name="title" id="title">
                           </div>
                           <div class="form-group label-floating">
                               <label class="control-label">Description</label>
-                              <input type="text" name="description" value="Titre du status" id="description" class="form-control">
+                              <input type="text" name="description" value="Sa description" id="description" class="form-control">
                           </div>
 
 
@@ -2489,16 +2647,24 @@ if (file_exists($target_dir)){
                         <input type="number" name="price" value="1" id="price" class="form-control">
                         </div>
 
+                        <div class="form-group form-file-upload">
+                            <input type="file" id="fileToUpload" name="fileToUpload[]" multiple="multiple">
+                            <div class="input-group">
+                                <input type="text" readonly="" class="form-control" placeholder="Insérer votre pièce jointe">
+                                <span class="input-group-btn input-group-s">
+                                    <button type="button" class="btn btn-just-icon btn-rose btn-round btn-info">
+                                        <i class="material-icons">layers</i>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+
                          </div>
                       </div>
 
                     <div class="col-sm-12">
                         <div class="card-content">
-
-                          <center>
-                          <button id="SubmitFormDataCreateUneActu" onclick="SubmitFormDataCreateUneActu();" type="button" class="btn btn-primary btn-round btn-rose">Créer</button>
-                          <button onclick="RetourIndex();" type="button" class="btn btn-primary btn-round btn-rose">Retour</button>
-                          </center>
+                            <input type="submit" name="submitactivite" value="Envoyer les images !">
                          </div>
                       </div>
                 </div>
