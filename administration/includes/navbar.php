17c769c847455d443b772e04bc80f2584281973b
Paul Boussard <boussard.paul@hotmail.com>
2019-01-01 19:19:04 +0100

    Update navbar.php

--- administration/includes/navbar.php
+++ administration/includes/navbar.php
@@ -1,71 +1,113 @@
-<nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
-	<div class="container">
-		<div class="navbar-translate">
-			<a class="navbar-brand" href="index.php" rel="tooltip">
-				Logo
-			</a>
-			<button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-bar top-bar"></span>
-				<span class="navbar-toggler-bar middle-bar"></span>
-				<span class="navbar-toggler-bar bottom-bar"></span>
-			</button>
-		</div>
-		<div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
-			<ul class="navbar-nav">
-				<?php
-				$cat = $db->query("SELECT DISTINCT name FROM sitecat");
-				while($unecat = $cat->fetch(PDO::FETCH_OBJ)){
-					$nom = $unecat->name
-					?>
-				<li class="nav-item dropdown">
-					<a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink1" data-toggle="dropdown">
-						<p><?php echo $nom ?></p>
-					</a>
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink1">
-						<?php
-						$souscat = $db->query("SELECT * FROM sitecat WHERE name = '$nom'");
-						while($unesouscat = $souscat->fetch(PDO::FETCH_OBJ)){
-						  ?>
-						<a class="dropdown-item" href="<?php echo $unesouscat->page;?>">
-						  <?php echo $unesouscat->surname ?>
-						</a>
-						<?php
-								}
-								?>
-					</li>
-						<?php
-						  }
-						if(!isset($_SESSION['admin_id'])){
-						?>
-						<li class="nav-item">
-	            <a class="nav-link" href="register.php">
-	              <i class="now-ui-icons files_single-copy-04"></i>
-	              <p>Inscription</p>
-	            </a>
-	          </li>
-						<li class="nav-item">
-	            <a class="nav-link" href="connect.php">
-	              <i class="now-ui-icons users_single-02"></i>
-	              <p>Se connecter</p>
-	            </a>
-	          </li>
-						<?php }else{ ?>
-						<li class="nav-item">
-							<a class="nav-link" href="https://dashboard.jam-mdm.fr/">
-								<i class="now-ui-icons users_circle-08"></i>
-								<p>Mon Compte</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="disconnect.php">
-								<i class="now-ui-icons arrows-1_share-66"></i>
-								<p>Deconnexion</p>
-							</a>
-						</li>
-						<?php } ?>
-					</div>
+<div class="sidebar" data-active-color="blue" data-background-color="black" data-image="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/worldfires-08232018.jpg">
+    <div class="logo">
+        <a href="https://jam-mdm.fr/" class="simple-text">
+            JAM             </a>
+    </div>
+    <div class="logo logo-mini">
+        <a href="https://jam-mdm.fr/" class="simple-text">
+            JAM
+        </a>
+    </div>
+    <div class="sidebar-wrapper">
+			<ul class="nav">
+<?php
+$cat = $db->query("SELECT * FROM admincat");
+while($unecat = $cat->fetch(PDO::FETCH_OBJ)){
+  ?>
+  <li
+  <?php
+  if($unecat->name == $nompage){
 
-			</ul>
-		</div>
-	</div>
-</nav>
+  ?>
+  class="active">
+  <?php }else{ ?>
+  >
+<?php } ?>
+
+      <a href="<?php echo $unecat->page;?>">
+          <i class="material-icons"><?php echo $unecat->icon;?></i>
+          <p><?php echo $unecat->name;?></p>
+      </a>
+      </li>
+
+		<?php } ?>
+
+            <li>
+                <a href="https://jam-mdm.fr/">
+                    <i class="material-icons">dns</i>
+                    <p>Retour JAM</p>
+                </a>
+            </li>
+            <li>
+                <a href="disconnect.php">
+                    <i class="material-icons">power_settings_new</i>
+                    <p>Déconnexion</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="main-panel">
+	<nav class="navbar navbar-transparent navbar-absolute">
+			<div class="container-fluid">
+					<div class="navbar-minimize">
+							<button id="minimizeSidebar" class="btn btn-round btn-white btn-fill btn-just-icon">
+									<i class="material-icons visible-on-sidebar-regular">more_vert</i>
+									<i class="material-icons visible-on-sidebar-mini">view_list</i>
+							</button>
+					</div>
+					<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#"> Charts </a>
+					</div>
+					<div class="collapse navbar-collapse">
+							<ul class="nav navbar-nav navbar-right">
+									<li>
+											<a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
+													<i class="material-icons">dashboard</i>
+													<p class="hidden-lg hidden-md">Dashboard</p>
+											</a>
+									</li>
+									<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+													<i class="material-icons">notifications</i>
+													<span class="notification">5</span>
+													<p class="hidden-lg hidden-md">
+															Notifications
+															<b class="caret"></b>
+													</p>
+											</a>
+											<ul class="dropdown-menu">
+													<li>
+															<a href="#">Mike John responded to your email</a>
+													</li>
+
+											</ul>
+									</li>
+									<li>
+											<a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
+													<i class="material-icons">person</i>
+													<p class="hidden-lg hidden-md">Profile</p>
+											</a>
+									</li>
+									<li class="separator hidden-lg hidden-md"></li>
+							</ul>
+							<form class="navbar-form navbar-right" role="search">
+									<div class="form-group form-search is-empty">
+											<input type="text" class="form-control" placeholder="Search">
+											<span class="material-input"></span>
+									</div>
+									<button type="submit" class="btn btn-white btn-round btn-just-icon">
+											<i class="material-icons">search</i>
+											<div class="ripple-container"></div>
+									</button>
+							</form>
+					</div>
+			</div>
+	</nav>
