23ec9b5470703d6af447f75fc285c2a5d3ac0aa8
Paul Boussard <boussard.paul@hotmail.com>
2019-01-01 19:51:44 +0100

    Update head.php

--- administration/includes/head.php
+++ administration/includes/head.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8" />
+  <meta charset="utf-8"/>
 
-  <meta name="Description" content="Association JAM ( Jeunesse Associative Montoise ) - Mont de Marsan">
+  <meta name="Description" content="Association JAM MDM (Jeunesse Associative Montoise), Mont de Marsan, IUT MDM, UPPA, association étudiante pour les étudiants de l'iut de Mont de Marsan developpé dans le cadre d'un projet pédagogique. Les activités de l'association comprennent des sorties, déjours ski, activités gratuite concernant différent thèmes. ">
   <meta name="Keywords" content="jam, association mont de marsan, iut mont de marsan, iut mdm, uppa">
   <meta name="Identifier-Url" content="https://jam-mdm.fr">
   <meta name="Reply-To" content="postmaster@jam-mdm.fr"> <!-- Mail Admin -->
@@ -16,13 +16,15 @@
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-  <title>Jam - <?php echo $nompage ?></title>
+  <title>Administration Jam - <?php echo $nompage ?></title>
 
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-  <!-- Les CSS utilisés -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
 
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-kit.css" rel="stylesheet"/>
+    <link href="assets/css/material-dashboard.css" rel="stylesheet"/>
 </head>
