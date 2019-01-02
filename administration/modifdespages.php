75b4b13fc32b1946e841a4c40238ef3417892c4f
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-01-02 14:58:15 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -1740,29 +1740,7 @@ function SubmitFormDataCreateUneActu() {
 
 </script>
 <?php
-//Création membres
-function slugify($text){
-$text = preg_replace('~[^\pL\d]+~u', '-', $text);
-
-$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-$text = preg_replace('~[^-\w]+~', '', $text);
-
-$text = trim($text, '-');
-
-$text = preg_replace('~-+~', '-', $text);
-
-$text = strtolower($text);
-
-if (empty($text)) {
-  return 'n-a';
-}
-
-  return $text;
-}
-
-
-
+//Création actualite
 if(isset($_POST['submitactualite'])){
 
 
