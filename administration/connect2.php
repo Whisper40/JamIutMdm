9e50a7c98cd6d753fac661d0f91a1b752d329879
Paul Boussard <boussard.paul@hotmail.com>
2019-01-02 18:06:11 +0100

    Update connect2.php

--- administration/connect2.php
+++ administration/connect2.php
@@ -262,19 +262,19 @@
                     </div>
                 </div>
             </div>
+            <footer class="footer">
+              <div class="container">
+                <center>
+                <div id="copyright">
+                  &copy;
+                  <script>
+                    document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+                  </script>, Jam - Jeunesse Associative Montoise - Créée par Paul Boussard et Kévin Perez
+                </div>
+              </center>
+              </div>
+            </footer>
         </div>
-        <footer class="footer">
-          <div class="container">
-            <center>
-            <div id="copyright">
-              &copy;
-              <script>
-                document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-              </script>, Jam - Jeunesse Associative Montoise - Créée par Paul Boussard et Kévin Perez
-            </div>
-          </center>
-          </div>
-        </footer>
     </div>
 </body>
 
