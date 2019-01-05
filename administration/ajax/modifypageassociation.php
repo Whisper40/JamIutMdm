5ed2db2ce595cc2acb95dfd7e36bb43fcd6a7867
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-01-05 10:48:28 +0100

    Update modifypageassociation.php

--- administration/ajax/modifypageassociation.php
+++ administration/ajax/modifypageassociation.php
@@ -1,7 +1,28 @@
 <?php
 require_once('../includes/connectBDD.php');
 
+        $user_id = $_POST['user_id'];
+        $id = $_POST['id'];
+        $titre1 = $_POST['titre1'];
+        $description1 = $_POST['description1'];
+        $description2 = $_POST['description2'];
+        $pagetitre = $_POST['pagetitre'];
+        $image = $_POST['image'];
 
+        if(!empty($user_id)&&!empty($id)&&!empty($titre1)&&!empty($description1)&&!empty($description2)&&!empty($pagetitre)&&!empty($image)){
+
+
+                $update = $db->prepare("UPDATE pageasso SET titre1=:titre1, description1=:description1, description2=:description2, etape3=:etape3 WHERE id=:id");
+                $update->execute(array(
+                    "id"=>$id,
+                    "titre1"=>$titre1,
+                    "description1"=>$description1,
+                    "description2"=>$description2,
+                    "etape3"=>$etape3
+                    )
+                );
+
+                
 
                 ?>
 
@@ -9,3 +30,16 @@ require_once('../includes/connectBDD.php');
                     demo.showSwal('success-message');
                     demo.showNotification('top','right','<b>Succès</b> - Modifications effectuées !');
                     </script>
+
+            <?php
+            }else{
+                ?>
+
+                    <script>
+                    demo.showSwal('danger-message');
+                    demo.showNotification('top','right','<b>Erreur</b> - Modifications non effectuées en raison de champs vides !');
+                    </script>
+            <?php
+            }
+
+    ?>
