485eb0acefa3b23b8fa82870c5faf03fc20d6b53
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-01-23 17:15:24 +0100

    Update deleteuneactu.php

--- administration/ajax/deleteuneactu.php
+++ administration/ajax/deleteuneactu.php
@@ -4,9 +4,44 @@ require_once('../includes/connectBDD.php');
         $user_id = $_POST['user_id'];
         $catactu = $_POST['catactu'];
 
-        if(!empty($user_id)&&!empty($catactu){
+        if(!empty($user_id)&&!empty($catactu)){
 
-          echo 'ok';
+          date_default_timezone_set('Europe/Paris');
+          setlocale(LC_TIME, 'fr_FR.utf8','fra');
+          $date = strftime('%d/%m/%Y %H:%M:%S');
+
+          $select = $db->prepare("SELECT slug FROM newsactus WHERE title=:catactu");
+          $select->execute(array(
+                              "catactu"=>$catactu
+                              )
+                          );
+          $sa = $select->fetch(PDO::FETCH_OBJ);
+          $slug=$sa->slug;
+
+
+
+          $delete = $db->prepare("DELETE * FROM newsactus WHERE slug=:slug");
+          $delete->execute(array(
+                              "slug"=>$slug
+                              )
+                          );
+
+                          $delete2 = $db->prepare("DELETE * FROM carousel WHERE slug=:slug");
+                          $delete2->execute(array(
+                                              "slug"=>$slug
+                                              )
+                                          );
+
+
+                                          $insertlogs = $db->prepare("INSERT INTO logs (user_id, type, action, page, date) VALUES(:user_id, :type, :action, :page, :date)");
+                                          $insertlogs->execute(array(
+                                                              "user_id"=>$user_id,
+                                                              "type"=>'Suppression',
+                                                              "action"=>'Suppression d une activite',
+                                                              "page"=>'activitees.php',
+                                                              "date"=>$date
+                                                              )
+                                                          );
 
             }else{
                 ?>
