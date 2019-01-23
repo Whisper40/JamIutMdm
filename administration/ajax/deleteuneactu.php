0193a31f66ddd11392179c34873a40dca907f9b8
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-01-23 17:11:20 +0100

    Update deleteuneactu.php

--- administration/ajax/deleteuneactu.php
+++ administration/ajax/deleteuneactu.php
@@ -1,26 +1,11 @@
 <?php
 require_once('../includes/connectBDD.php');
 
-        $user_id = $_POST['user_id'];
-        $catactu = $_POST['catactu'];
-
-        if(!empty($user_id)&&!empty($catactu){
-
-          date_default_timezone_set('Europe/Paris');
-          setlocale(LC_TIME, 'fr_FR.utf8','fra');
-          $date = strftime('%d/%m/%Y %H:%M:%S');
-
-          $select = $db->prepare("SELECT slug FROM newsactus WHERE title=:catactu");
-          $select->execute(array(
-                              "catactu"=>$catactu
-                              )
-                          );
-          $sa = $select->fetch(PDO::FETCH_OBJ);
-          $slug=$sa->slug;
+      
 
 
 
-          
+
             }else{
                 ?>
 
