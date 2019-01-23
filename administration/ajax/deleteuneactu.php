2f0d1e96085dec2636e4fd476dc6bb8da9702c3f
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-01-23 17:14:47 +0100

    Update deleteuneactu.php

--- administration/ajax/deleteuneactu.php
+++ administration/ajax/deleteuneactu.php
@@ -1,4 +1,20 @@
 <?php
-echo 'ok';
+require_once('../includes/connectBDD.php');
+
+        $user_id = $_POST['user_id'];
+        $catactu = $_POST['catactu'];
+
+        if(!empty($user_id)&&!empty($catactu){
+
+          echo 'ok';
+
+            }else{
+                ?>
+
+                    <script>
+                    demo.showNotification('top','right','<b>Erreur</b> - Création non effectuée en raison de champs vides !');
+                    </script>
+            <?php
+            }
 
     ?>
