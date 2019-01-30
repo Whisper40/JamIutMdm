913ea3d603b36363b238648acdb72e43ef963921
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-01-30 17:38:20 +0100

    Update index.php

--- administration/index.php
+++ administration/index.php
@@ -317,8 +317,8 @@ $valyear0 = date("Y");
 
 
 
-$maxvalue = max($countmonth5,$countmonth4,$countmonth3,$countmonth2,$countmonth1,$countmonth0);
-$maxavecmarge = round($maxvalue);
+    $maxvalue = max($countmonth5,$countmonth4,$countmonth3,$countmonth2,$countmonth1,$countmonth0);
+    $maxavecmarge = round($maxvalue + 1);
 
 
 //simpleBarChart
