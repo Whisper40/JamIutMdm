73c8496db1193f994708fcab90f664d45dadd64c
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-02-03 23:22:35 +0100

    Update affichelesparticipants.php

--- administration/affichelesparticipants.php
+++ administration/affichelesparticipants.php
@@ -127,6 +127,23 @@ if(isset($_GET['action'])){
       fputcsv($fp, $fields);
     }
     fclose($fp);
+
+
+
+    $list = array (
+    array('aaa', 'bbb', 'ccc', 'dddd'),
+    array('123', '456', '789'),
+    array('"aaa"', '"bbb"')
+);
+
+$fp = fopen('file.csv', 'w');
+
+foreach ($list as $fields) {
+    fputcsv($fp, $fields);
+}
+
+fclose($fp);
+?>
   }
 }
 
