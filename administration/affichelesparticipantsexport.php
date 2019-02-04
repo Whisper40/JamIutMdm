089b479a813225ca66e3335c91bd11d9c66b63a8
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-02-04 22:09:11 +0100

    Update affichelesparticipantsexport.php

--- administration/affichelesparticipantsexport.php
+++ administration/affichelesparticipantsexport.php
@@ -26,10 +26,6 @@ $tableau = array();
 
 
 
-header("Content-Type: text/csv");
-header("Content-disposition: filename=Tableau-Ski-$date.csv");
-
-
 
 
 
@@ -102,6 +98,117 @@ foreach ($tableau as $ligne) {
 
 
 }else if (stripos($title, 'rugby') != FALSE){
+
+
+
+
+
+
+
+
+  header('Content-Type: text/csv; charset=utf-8');
+  header("Content-disposition: filename=Tableau-Rugby-$date.csv");
+
+  date_default_timezone_set('Europe/Paris');
+  setlocale(LC_TIME, 'fr_FR.utf8','fra');
+  $date = strftime('%d:%m:%y %H:%M:%S');
+
+  $tableau = array();
+
+
+
+  $selectid = $db->prepare("SELECT user_id FROM catparticipe WHERE name=:name");
+  $selectid->execute(array(
+    "name"=>$title
+  ));
+  $nbr = $selectid->rowCount();
+
+  while($s0=$selectid->fetch(PDO::FETCH_OBJ)){
+    $iddelapersonne=$s0->user_id;
+
+    $selectnom = $db->prepare("SELECT username, prenom, email FROM users WHERE id=:id");
+    $selectnom->execute(array(
+      "id"=>$iddelapersonne
+    ));
+    $snom=$selectnom->fetch(PDO::FETCH_OBJ);
+    $nom=$snom->username;
+    $email=$snom->email;
+    $prenom=$snom->prenom;
+
+    $selectinfos = $db->prepare("SELECT * FROM participe WHERE user_id=:id and activity_name=:name");
+    $selectinfos->execute(array(
+      "id"=>$iddelapersonne,
+      "name"=>$slug
+    ));
+
+    while($s1=$selectinfos->fetch(PDO::FETCH_OBJ)){
+
+        $optionaccompagnement=$s1->optionaccompagnement;
+
+
+      $selectinfospersonnelles = $db->prepare("SELECT * FROM formulairerugby WHERE user_id=:id");
+      $selectinfospersonnelles->execute(array(
+        "id"=>$iddelapersonne
+      ));
+
+      while($s2=$selectinfospersonnelles->fetch(PDO::FETCH_OBJ)){
+        $adresse=$s2->adresse;
+        $codepostal=$s2->codepostal;
+        $ville=$s2->ville;
+        $tel=$s2->tel;
+        $telurgence=$s2->telurgence;
+        $regroupement = $codepostal.' '.$ville;
+
+        $tableau[] = array($nom,$prenom,$email,$adresse,$regroupement,$tel,$telurgence,$optionaccompagnement);
+
+
+      }
+    }
+  }
+
+
+  $entete = array("Nom", "Prenom", "Email", "Adresse", "CP", "Telephone", "Telephone Urgence", "Option Accompagnement");
+
+
+  $separateur = ";";
+  // Affichage de la ligne de titre, terminée par un retour chariot
+  echo implode($separateur, $entete)."\r\n";
+
+  // Affichage du contenu du tableau
+  foreach ($tableau as $ligne) {
+    echo implode($separateur, $ligne)."\r\n";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
