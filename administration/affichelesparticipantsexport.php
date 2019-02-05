6617e3580c724300d0686e23b0c85bd438245492
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-02-05 23:17:38 +0100

    Update affichelesparticipantsexport.php

--- administration/affichelesparticipantsexport.php
+++ administration/affichelesparticipantsexport.php
@@ -13,16 +13,16 @@ $slug = $_GET['slug'];
 $title = $_GET['title'];
 
 
-function strpos_arr($haystack, $needle) {
-    if(!is_array($needle)) $needle = array($needle);
-    foreach($needle as $what) {
-        if(($pos = strpos($haystack, $what))!==false) return $pos;
-    }
-    return false;
+function replaceAccents($str) {
+
+$search = explode(",","ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,ø,Ø,Å,Á,À,Â,Ä,È,É,Ê,Ë,Í,Î,Ï,Ì,Ò,Ó,Ô,Ö,Ú,Ù,Û,Ü,Ÿ,Ç,Æ,Œ");
+
+$replace = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,o,O,A,A,A,A,A,E,E,E,E,I,I,I,I,O,O,O,O,U,U,U,U,Y,C,AE,OE");
+
+return str_replace($search, $replace, $str);
 }
 
-//SPECIAL
-$arraycinema = array("cinema", "cinéma", "cinèma");
+$newtitle = replaceAccents($title);
 
 //FIN
 
@@ -195,7 +195,7 @@ foreach ($tableau as $ligne) {
 
 
 
-}else if (stripos($title, 'cinema') != FALSE){
+}else if (stripos($newtitle, 'cinema') != FALSE){
 
 
 
