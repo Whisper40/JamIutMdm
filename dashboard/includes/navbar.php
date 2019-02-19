8f4938d3a5d21953b80c534cfa636d41d9f27c28
Paul Boussard <boussard.paul@hotmail.com>
2019-02-19 22:34:11 +0100

    Update navbar.php

--- dashboard/includes/navbar.php
+++ dashboard/includes/navbar.php
@@ -135,39 +135,3 @@ if($countstatusmembre == '1'){
 </div>
 
 <div class="main-panel">
-    <nav class="navbar navbar-transparent navbar-absolute">
-        <div class="container-fluid">
-            <div class="navbar-minimize">
-                <button id="minimizeSidebar" class="btn btn-round btn-white btn-fill btn-just-icon">
-                    <i class="material-icons visible-on-sidebar-regular">more_vert</i>
-                    <i class="material-icons visible-on-sidebar-mini">view_list</i>
-                </button>
-            </div>
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="https://dashboard.jam-mdm.fr"> Dashboard </a>                    </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                                                <i class="material-icons">notifications</i>
-                                                                <p class="hidden-lg hidden-md">
-                                Notifications
-                                <b class="caret"></b>
-                            </p>
-                        </a>
-                        <ul class="dropdown-menu" id="menu_notifications">
-                            <li>
-                                        <a>Aucune notification</a>
-                                    </li>                               </ul>
-                    </li>
-                    <li class="separator hidden-lg hidden-md"></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
