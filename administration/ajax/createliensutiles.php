c3477f6a19a14dbe657c14abacb1f81334de0492
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-02-19 21:43:41 +0100

    Update createliensutiles.php

--- administration/ajax/createliensutiles.php
+++ administration/ajax/createliensutiles.php
@@ -11,15 +11,7 @@ require_once('../includes/connectBDD.php');
 
         if(!empty($user_id)&&!empty($nom)&&!empty($description)&&!empty($lienimage)&&!empty($lien)&&!empty($catslug)){
 
-                $insert = $db->prepare("INSERT INTO liensutiles (slug, name, description, lienimage, lien) VALUES (:catslug, :nom, :description, :lienimage, :lien)");
-                $insert->execute(array(
-                    "catslug"=>$catslug,
-                    "nom"=>$nom,
-                    "description"=>$description,
-                    "lienimage"=>$lienimage,
-                    "lien"=>$lien
-                    )
-                );
+              
 
                 date_default_timezone_set('Europe/Paris');
                 setlocale(LC_TIME, 'fr_FR.utf8','fra');
