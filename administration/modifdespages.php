242e5a45771c73c899b0569d0db3985d82487c37
Paul Boussard <boussard.paul@hotmail.com>
2019-02-19 18:16:13 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -3520,7 +3520,7 @@ $target_file3 = $target_dirnew."".$slug.".".$imageFileType;
                     <input onclick="document.getElementById('description1').value=document.getElementById('description1').value+'<i>Texte en Italic</i>'" type="button" name="Italic" id="Italic" value="Italic" class="btn btn-rose btn-round btn-sm"/>
                     <input onclick="document.getElementById('description1').value=document.getElementById('description1').value+'<sub>Texte en Indice</sub>'" type="button" name="Indice" id="Indice" value="Indice" class="btn btn-rose btn-round btn-sm"/>
                     <input onclick="document.getElementById('description1').value=document.getElementById('description1').value+'<mark>Texte Surligné</mark>\n\n\n\n<style>\nmark { \nbackground-color: red; <-- couleur surlignage -->\ncolor: black; <-- couleur du texte -->\n}\n</style>'" type="button" name="Surligné" id="Surligné" value="Surligné" class="btn btn-rose btn-round btn-sm"/>
-                    <br>
+                    <br><br>
                     <button type="submit" name="submitactualite" class="btn btn-primary btn-round btn-rose">Créer une actualité</button>
                   </center>
                 </div>
@@ -5110,91 +5110,87 @@ if ($uploadOk == 0) {
           } ?>
 
 
-
-<div class="content">
-    <div class="container-fluid">
-        <div class="card">
-            <div class="card-content">
-                <h2 class="card-title text-center">Modification de la page Galerie</h2>
-
-
-
-
-
-
-
-<form  method="POST" class="form-horizontal"  enctype="multipart/form-data">
-                              <div class="row">
-                                  <div class="col-sm-6">
-
-                                       <div class="form-group form-file-upload">
-                                           <input type="file" id="fileToUpload" name="fileToUpload[]" multiple="multiple">
-                                           <div class="input-group">
-                                               <input type="text" readonly="" class="form-control" placeholder="<?php echo $image; ?>">
-                                               <span class="input-group-btn input-group-s">
-                                                   <button type="button" class="btn btn-just-icon btn-rose btn-round btn-info">
-                                                       <i class="material-icons">layers</i>
-                                                   </button>
-                                               </span>
-                                           </div>
-                                       </div>
-                                    </div>
-
-                                    <div class="col-sm-12">
-                                        <div class="card-content">
-                                            <input type="submit" name="modifphotogalerie" value="Modifier l'image">
-                                         </div>
-                                      </div>
-                              </div>
-                            </form>
-
-
-
-                <form action="" method="post" id="myForm1" class="contact-form">
-                <div class="row">
-                    <div class="col-sm-6">
+          <div class="content">
+            <div class="container-fluid">
+              <div class="card">
+                <div class="card-content">
+                  <h2 class="card-title text-center">Modification page Galerie</h2>
+                    <div class="row">
+                      <div class="col-sm-12">
                         <div class="card-content">
-                          <div class="form-group label-floating">
-                              <label class="control-label">Titre de la page</label>
-                              <input type="text" class="form-control" value="<?php echo $pagetitre; ?>" name="pagetitre" id="pagetitre">
-                          </div>
-
-                        <div class="form-group label-floating">
-                        <label class="control-label">Titre</label>
-                        <input type="text" name="titre" value="<?php echo $titre;?>" id="titre" class="form-control">
+                          <h3 class="card-title">En-tête de page</h3>
                         </div>
-
-
-
-                         </div>
                       </div>
-
-                    <div class="col-sm-12">
+                    </div>
+                    <div class="row">
+                      <div class="col-sm-6">
                         <div class="card-content">
-
-                          <center>
-                          <button id="submitFormDataGallerie" onclick="SubmitFormDataGallerie();" type="button" class="btn btn-primary btn-round btn-rose">Modifier</button>
-                          <button onclick="RetourIndex();" type="button" class="btn btn-primary btn-round btn-rose">Retour</button>
-                          </center>
-                         </div>
+                          <form  method="POST" class="form-horizontal"  enctype="multipart/form-data">
+                            <h3 class="card-title text-center">Image d'Arrière plan</h3>
+                            <br><br><br>
+                            <center>
+                              <div class="fileinput fileinput-new text-center" data-provides="fileinput">
+                                <div class="fileinput-new thumbnail">
+                                  <img src="https://jam-mdm.fr/JamFichiers/Img/ImagesDuSite/Original/<?php echo $image; ?>" alt="...">
+                                </div>
+                                <div class="fileinput-preview fileinput-exists thumbnail"></div>
+                                <br><br>
+                                <div>
+                                  <span class="btn btn-rose btn-round btn-file">
+                                    <span class="fileinput-new">Selection image</span>
+                                    <span class="fileinput-exists">Changer</span>
+                                    <input type="file" id="fileToUpload" name="fileToUpload[]" multiple="multiple">
+                                  </span>
+                                  <a href="#pablo" class="btn btn-danger btn-round fileinput-exists" data-dismiss="fileinput"><i class="fa fa-times"></i> Annulé</a>
+                                  <button type="submit" name="modifphotogalerie" class="btn btn-primary btn-round btn-rose">Modifier l'image</button>
+                                </div>
+                              </div>
+                            </center>
+                          </form>
+                        </div>
                       </div>
-                </div>
-              </form>
-            </div>
-        </div>
-    </div>
-
- <div id="results22"> <!-- TRES IMPORTANT -->
-</div>
-</div>
-
-
-<h3> Pour ajouter des images concernant la galerie, c'est ici : </h3>
-<a href="https://administration.jam-mdm.fr/ajoutimage.php" target="_blank" class="w3-button w3-black">Ajouter des images</a>
-
-
-<h3> Pour gérer les images concernant la galerie, c'est ici : </h3>
-<a href="https://administration.jam-mdm.fr/gestionimage.php" target="_blank" class="w3-button w3-black">Gérer les images</a>
+                    <div class="col-sm-6">
+                      <div class="card-content">
+                        <form action="" method="post" id="myForm1" class="contact-form">
+                          <h3 class="card-title text-center">Titres de la page</h3>
+                          <div class="form-group label-floating">
+                            <label class="control-label">Titre de la page</label>
+                            <input type="text" class="form-control" value="<?php echo $pagetitre; ?>" name="pagetitre" id="pagetitre">
+                          </div>
+                          <div class="form-group label-floating">
+                            <label class="control-label">Titre</label>
+                            <input type="text" name="titre" value="<?php echo $titre;?>" id="titre" class="form-control">
+                          </div>
+                          <br>
+                          <center>
+                            <button id="submitFormDataGallerie" onclick="SubmitFormDataGallerie();" type="button" class="btn btn-primary btn-round btn-rose">Modifier</button>
+                          </center>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="results22"></div>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="card-content">
+                        <h3 class="card-title">Gérer les images</h3>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="card-content">
+                        <h3> Pour ajouter des images concernant la galerie, c'est ici : </h3>
+                        <a href="https://administration.jam-mdm.fr/ajoutimage.php" target="_blank" class="w3-button w3-black">Ajouter des images</a>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="card-content">
+                        <h3> Pour gérer les images concernant la galerie, c'est ici : </h3>
+                        <a href="https://administration.jam-mdm.fr/gestionimage.php" target="_blank" class="w3-button w3-black">Gérer les images</a>
+                      </div>
+                    </div>
+                  </div>
 
 
 <?php
