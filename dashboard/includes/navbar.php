9187fa98b8b569f3a3f61bf0abdc8326e1e36528
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-02-20 08:57:46 +0100

    Update navbar.php

--- dashboard/includes/navbar.php
+++ dashboard/includes/navbar.php
@@ -143,7 +143,31 @@ if($countstatusmembre == '1'){
                     <i class="material-icons visible-on-sidebar-mini">view_list</i>
                 </button>
             </div>
-            
-
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="https://dashboard.jam-mdm.fr"> Dashboard </a>                    </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                                                <i class="material-icons">notifications</i>
+                                                                <p class="hidden-lg hidden-md">
+                                Notifications
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <ul class="dropdown-menu" id="menu_notifications">
+                            <li>
+                                        <a>Aucune notification</a>
+                                    </li>                               </ul>
+                    </li>
+                    <li class="separator hidden-lg hidden-md"></li>
+                </ul>
+            </div>
         </div>
     </nav>
