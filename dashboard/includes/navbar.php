359abe55384d0a574b79168adae900708c9c0b4d
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-02-20 08:57:26 +0100

    Update navbar.php

--- dashboard/includes/navbar.php
+++ dashboard/includes/navbar.php
@@ -135,4 +135,15 @@ if($countstatusmembre == '1'){
 </div>
 
 <div class="main-panel">
-  
+    <nav class="navbar navbar-transparent navbar-absolute">
+        <div class="container-fluid">
+            <div class="navbar-minimize">
+                <button id="minimizeSidebar" class="btn btn-round btn-white btn-fill btn-just-icon">
+                    <i class="material-icons visible-on-sidebar-regular">more_vert</i>
+                    <i class="material-icons visible-on-sidebar-mini">view_list</i>
+                </button>
+            </div>
+            
+
+        </div>
+    </nav>
