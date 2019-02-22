d840cf3b15daed2afd5c69d7c3dfa42f56d208ef
Paul Boussard <boussard.paul@hotmail.com>
2019-02-22 18:30:45 +0100

    Update gestionimage.php

--- administration/gestionimage.php
+++ administration/gestionimage.php
@@ -63,12 +63,7 @@ $('#resultat').html(retour).fadeIn();
 });
 </script>
 
-<body>
-  <div class="wrapper">
-
 <?php
-
-    require_once('includes/navbar.php');
 
 if($_GET['action']=='unban'){
 
@@ -112,7 +107,8 @@ if (file_exists($target_dir.'/'.$original.'/'.$dossier)){
 
 }else{
   echo 'n extse pas';
-  $error = 'Un problème de répertoire est présent, contacter votre administrateur !';
+  $$messagenotif = 'Un problème de répertoire est présent, contacter votre administrateur !';
+  $type = "warning";
 }
 echo 'esquive';
 
@@ -121,98 +117,7 @@ echo 'esquive';
 <?php
 } ?>
 
-          <div class="content">
-            <div class="container-fluid">
-              <div class="card">
-                <div class="card-content">
-                  <h2 class="card-title text-center">Gérer les images de la galerie</h2>
-                  <br>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <div class="card-content">
-                        <h3 class="card-title">Liste des images inactives</h3>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <div class="card-content">
-
-          <?php
-
-          $selectbanimg = $db->prepare("SELECT * FROM images WHERE status='0'");
-          $selectbanimg->execute();
-          $countbanimg = $selectbanimg->rowCount();
-          if($countbanimg>'0'){
-          ?>
-
-                  <div class="table-responsive">
-                    <table class="table">
-                      <thead class="text-primary">
-                        <th class="text-center">Identifiant</th>
-                        <th class="text-center">Nom</th>
-                        <th class="text-center">Catégorie</th>
-                        <th class="text-center">Album Actif</th>
-                        <th class="text-center">Action</th>
-                      </thead>
-                      <tbody>
-
-                        <?php
-                            while($sban=$selectbanimg->fetch(PDO::FETCH_OBJ)){
-                              $idimg = $sban->id;
-                              $file_name = $sban->file_name;
-                              $title = $sban->title;
-                              $albumactif = $sban->albumactif;
-                        ?>
-
-                        <tr>
-                          <td class="text-center"><?php echo $idimg;?></td>
-                          <td class="text-center"><?php echo $file_name;?></td>
-                          <td class="text-center"><?php echo $title;?></td>
-                          <td class="text-center"><?php echo $albumactif;?></td>
-                          <td class="text-center"><a href="?action=unban&amp;id=<?php echo $idimg;?>"><button type="button" class="btn btn-rose btn-round btn-sm">Activer</button></a>
-                                                  <a href="?action=delete&amp;id=<?php echo $idimg;?>"><button type="button" class="btn btn-rose btn-round btn-sm">Supprimer</button></a>
-                          </td>
-                        </tr>
-
-                        <?php } ?>
-
-                      </tbody>
-                    </table>
-                  </div>
-
-                  <?php }else{?>
-
-                   <center>
-                      <h4 class="info-title"><font color="red">Aucun image n'est actuellement bannie</font></h4>
-                  </center>
-
-                  <?php } ?>
-
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-12">
-                <div class="card-content">
-                  <h3 class="card-title">Desactiver une ou plusieurs images</h3>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6 col-md-offset-3">
-                <div class="card-content">
-                  <input type="text" class="form-control"  name="valeur" placeholder="Recherche par Nom, Identifiant ou Catégorie">
-                </div>
-              </div>
-              <div class="col-sm-12">
-                <div class="card-content">
-                  <p id='resultat'></p>
-                </div>
-              </div>
-            </div>
-
- <?php
+<?php
 
 
 if(isset($_POST['submit'])){
@@ -220,73 +125,171 @@ $action = $_POST['optionsRadios'];
 $cat = $_POST['catimage'];
 if($action == 'defaut'){
 
-  $insertinfos = $db->prepare("UPDATE images SET albumactif=:albumactif, status=:status WHERE title=:title");
-  $insertinfos->execute(array(
-      "title"=>$cat,
-      "albumactif"=>'1',
-      "status"=>'1'
-      )
-  );
+ $insertinfos = $db->prepare("UPDATE images SET albumactif=:albumactif, status=:status WHERE title=:title");
+ $insertinfos->execute(array(
+     "title"=>$cat,
+     "albumactif"=>'1',
+     "status"=>'1'
+     )
+ );
 
-  $success = "L'album à été définis comme album par défaut";
+ $$messagenotif = "L'album à été définis comme album par défaut";
+ $type = "success";
 
 }else if ($action == 'ban'){
 
-  $insertinfos = $db->prepare("UPDATE images SET albumactif=:albumactif, status=:status WHERE title=:title");
-  $insertinfos->execute(array(
-      "title"=>$cat,
-      "albumactif"=>'0',
-      "status"=>'0'
-      )
-  );
+ $insertinfos = $db->prepare("UPDATE images SET albumactif=:albumactif, status=:status WHERE title=:title");
+ $insertinfos->execute(array(
+     "title"=>$cat,
+     "albumactif"=>'0',
+     "status"=>'0'
+     )
+ );
 
-  $success = "L'album à été définis comme album par défaut";
+ $$messagenotif = "L'album à été définis comme album par défaut";
+ $type = "success";
 }else if ($action == 'delete'){
 
-  $dossier = $cat;
+ $dossier = $cat;
 
-  $target_dir = '../../../JamFichiers/Photos';
-  $original = 'Original';
-  $affiche = 'Affiche';
-  $thumb = 'Thumb';
+ $target_dir = '../../../JamFichiers/Photos';
+ $original = 'Original';
+ $affiche = 'Affiche';
+ $thumb = 'Thumb';
 
-  if (file_exists($target_dir.'/'.$original.'/'.$dossier)){
+ if (file_exists($target_dir.'/'.$original.'/'.$dossier)){
 
-    function removeDirectory($path) {
- 	      $files = glob($path . '/*');
-	      foreach ($files as $file) {
-		        is_dir($file) ? removeDirectory($file) : unlink($file);
-	         }
-	          rmdir($path);
- 	          return;
+   function removeDirectory($path) {
+       $files = glob($path . '/*');
+       foreach ($files as $file) {
+           is_dir($file) ? removeDirectory($file) : unlink($file);
           }
+           rmdir($path);
+           return;
+         }
 
 removeDirectory("$target_dir/$original/$dossier");
 removeDirectory("$target_dir/$affiche/$dossier");
 removeDirectory("$target_dir/$thumb/$dossier");
 
 
-    $updatedelete = $db->prepare("DELETE FROM images WHERE title=:title");
-    $updatedelete->execute(array(
-        "title"=>$cat
-        )
-    );
+   $updatedelete = $db->prepare("DELETE FROM images WHERE title=:title");
+   $updatedelete->execute(array(
+       "title"=>$cat
+       )
+   );
 
-  }else{
-    $error = "Le répertoire n\'existe pas ! ";
-  }
+ }else{
+   $$messagenotif = "Le répertoire n\'existe pas ! ";
+   $type = "warning";
+ }
 
 }else{
-  $error = "Aucune action sélectionnée ";
+ $$messagenotif = "Aucune action sélectionnée ";
+ $type = "warning";
 }
 
 
 }
 
+ ?>
 
-$selectcatimages=$db->query("SELECT DISTINCT title FROM images");
+<body>
+  <div class="wrapper">
 
-  ?>
+    <?php
+    require_once('includes/navbar.php');
+    ?>
+
+    <div class="content">
+      <div class="container-fluid">
+        <div class="card">
+          <div class="card-content">
+            <h2 class="card-title text-center">Gérer les images de la galerie</h2>
+            <br>
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="card-content">
+                  <h3 class="card-title">Liste des images inactives</h3>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="card-content">
+
+                <?php
+                $selectbanimg = $db->prepare("SELECT * FROM images WHERE status='0'");
+                $selectbanimg->execute();
+                $countbanimg = $selectbanimg->rowCount();
+                if($countbanimg>'0'){
+                ?>
+
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class="text-primary">
+                      <th class="text-center">Identifiant</th>
+                      <th class="text-center">Nom</th>
+                      <th class="text-center">Catégorie</th>
+                      <th class="text-center">Album Actif</th>
+                      <th class="text-center">Action</th>
+                    </thead>
+                    <tbody>
+
+                      <?php
+                      while($sban=$selectbanimg->fetch(PDO::FETCH_OBJ)){
+                        $idimg = $sban->id;
+                        $file_name = $sban->file_name;
+                        $title = $sban->title;
+                        $albumactif = $sban->albumactif;
+                      ?>
+
+                    <tr>
+                      <td class="text-center"><?php echo $idimg;?></td>
+                      <td class="text-center"><?php echo $file_name;?></td>
+                      <td class="text-center"><?php echo $title;?></td>
+                      <td class="text-center"><?php echo $albumactif;?></td>
+                      <td class="text-center"><a href="?action=unban&amp;id=<?php echo $idimg;?>"><button type="button" class="btn btn-rose btn-round btn-sm">Activer</button></a>
+                                              <a href="?action=delete&amp;id=<?php echo $idimg;?>"><button type="button" class="btn btn-rose btn-round btn-sm">Supprimer</button></a>
+                      </td>
+                    </tr>
+
+                    <?php } ?>
+
+                  </tbody>
+                </table>
+              </div>
+
+              <?php }else{?>
+
+              <center>
+                <h4 class="info-title"><font color="red">Aucun image n'est actuellement bannie</font></h4>
+              </center>
+
+              <?php } ?>
+
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="card-content">
+              <h3 class="card-title">Desactiver une ou plusieurs images</h3>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <div class="card-content">
+              <input type="text" class="form-control"  name="valeur" placeholder="Recherche par Nom, Identifiant ou Catégorie">
+            </div>
+          </div>
+          <div class="col-sm-12">
+            <div class="card-content">
+              <p id='resultat'></p>
+            </div>
+          </div>
+        </div>
         <div class="row">
           <div class="col-sm-12">
             <div class="card-content">
@@ -317,6 +320,7 @@ $selectcatimages=$db->query("SELECT DISTINCT title FROM images");
                    <div class="jquerysel">
                      <select class="selectpicker" data-style="select-with-transition" title="Fonction" data-size="7" name="catimage">
                        <?php
+                        $selectcatimages=$db->query("SELECT DISTINCT title FROM images");
                          while($s = $selectcatimages->fetch(PDO::FETCH_OBJ)){
                            $catimage=$s->title;
                         ?>
@@ -359,7 +363,7 @@ $selectcatimages=$db->query("SELECT DISTINCT title FROM images");
                  </div>
                </div>
              </div>
-           </form>     
+           </form>
          </div>
        </div>
      </div>
