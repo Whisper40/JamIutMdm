e23914ad07687722308949e10e88f9cf9cc19520
PEREZ KEVIN <perez40390@hotmail.fr>
2019-03-09 09:36:34 +0100

    test 2

--- administration/ajax/createmembreexterne.php
+++ administration/ajax/createmembreexterne.php
@@ -48,7 +48,7 @@ require_once('../includes/connectBDD.php');
                                        $headers = "From: <".$owner_mail.">\r\n";
                                        $headers .= "MIME-Version: 1.0\r\n";
                                        $headers .= 'X-Priotity:'.$priority."\r\n";
-                                       $headers .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+                                       $headers .= "Content-Type: text/html\r\n";
 
 
 
