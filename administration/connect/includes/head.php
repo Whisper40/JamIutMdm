d86924095cd41dee7a82d6ef8f22a42c4aded1b4
Paul Boussard <boussard.paul@hotmail.com>
2019-03-09 20:34:15 +0100

    assets

--- administration/connect/includes/head.php
+++ administration/connect/includes/head.php
@@ -12,8 +12,8 @@
   <meta name="Rating" content="general">
   <meta name="Distribution" content="global">
   <meta name="Category" content="internet">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="connect/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="connect/assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
   <title>Jam - <?php echo $nompage ?></title>
@@ -23,6 +23,6 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
 
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
+  <link href="connect/assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="connect/assets/css/now-ui-kit.css?v=1.2.0" rel="stylesheet" />
 </head>
