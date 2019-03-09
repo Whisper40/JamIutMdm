36fb6567204653f7fc0bde6d974a0cb649fb2736
Paul Boussard <boussard.paul@hotmail.com>
2019-03-09 12:53:28 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -2948,9 +2948,13 @@ if (file_exists($target_dir)){
                   <div class="jquerysel">
                     <select class="selectpicker souscatactualite" data-style="select-with-transition" title="Sous Catégorie" data-size="7" name="souscatactualite">
                       <option disabled>Choisir une sous-categorie</option>
-
                     </select>
                   </div>
+
+                  Sous Catégorie :
+            <select name="souscatactualite" class="souscatactualite">
+<option>Sélectionner la sous catégorie</option>
+</select>
 
                 </div>
               </div>
