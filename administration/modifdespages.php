413077a92d7ba7daca8006595f643e5303807ead
Paul Boussard <boussard.paul@hotmail.com>
2019-03-17 13:25:31 +0100

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -2883,7 +2883,6 @@ if (file_exists($target_dir)){
                     <br>
                     <center>
                       <button id="submitFormDataModifActualite" onclick="SubmitFormDataModifActualite();" type="button" class="btn btn-primary btn-round btn-rose">Modifier</button>
-                      <button onclick="RetourIndex4();" type="button" class="btn btn-primary btn-round btn-rose">Retour</button>
                     </center>
                   </div>
                 </div>
@@ -2902,28 +2901,26 @@ if (file_exists($target_dir)){
               <div class="row">
                 <div class="col-sm-6">
                   <div class="card-content">
-
+                    <br>
                     <div class="jquerysel">
                       <select class="selectpicker souscatactualite" data-style="select-with-transition" title="Catégorie" data-size="7" name="souscatactualite">
                         <option disabled>Choisir une categorie</option>
-
-                    <?php
-                          $s5=$db->query("SELECT * FROM newsactus where id='$id'");
-                          $row = $s5->fetch(PDO::FETCH_OBJ);
-                          $title = $row->title;
-                          $title2 = $row->title2;
-                          $title3 = $row->title3;
-                    				echo '<option value="'.$title.'">'.$title.'</option>';
-                            if(!empty($title2)){
-                            echo '<option value="'.$title2.'">'.$title2.'</option>';
-                            }
-                            if(!empty($title3)){
-                            echo '<option value="'.$title3.'">'.$title3.'</option>';
-                            }
+                      <?php
+                      $s5=$db->query("SELECT * FROM newsactus where id='$id'");
+                      $row = $s5->fetch(PDO::FETCH_OBJ);
+                      $title = $row->title;
+                      $title2 = $row->title2;
+                      $title3 = $row->title3;
+                				echo '<option value="'.$title.'">'.$title.'</option>';
+                        if(!empty($title2)){
+                        echo '<option value="'.$title2.'">'.$title2.'</option>';
+                        }
+                        if(!empty($title3)){
+                        echo '<option value="'.$title3.'">'.$title3.'</option>';
+                        }
                       ?>
                       </select>
                     </div>
-
                   </div>
                 </div>
                 <div class="col-sm-6">
@@ -2995,6 +2992,15 @@ if (file_exists($target_dir)){
                 </div>
               </div>
               <br>
+            </div>
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="card-content">
+                  <center>
+                    <button onclick="RetourIndex4();" type="button" class="btn btn-primary btn-round btn-rose">Retour</button>
+                  </center>
+                </div>
+              </div>
             </div>
           </div>
         </div>
