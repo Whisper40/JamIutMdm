05e4a08a45ae8db2043e4d3d0e949ed9673831b7
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-03-17 21:47:55 +0100

    Update modifypasswordpanel.php

--- administration/ajax/modifypasswordpanel.php
+++ administration/ajax/modifypasswordpanel.php
@@ -8,7 +8,7 @@ require_once('../includes/connectBDD.php');
 
         if(!empty($email)&&(!empty($password))&&(!empty($repeatpassword))){
             if($password==$repeatpassword){
-                 if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{10,}$#', $password)){
+                 if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{5,}$#', $password)){
                 $user_id = $_SESSION['admin_id'];
                 date_default_timezone_set('Europe/Paris');
                 setlocale(LC_TIME, 'fr_FR.utf8','fra');
