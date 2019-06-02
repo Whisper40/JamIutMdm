47160c6e47e12b1e6bcb93af185d9676942ab835
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-06-02 20:47:09 +0200

    Update addimage.php

--- administration/ajax/addimage.php
+++ administration/ajax/addimage.php
@@ -6,27 +6,26 @@ define('MB', 1048576);
 define('GB', 1073741824);
 define('TB', 1099511627776);
 $test = $_POST['catimage'];
-echo $test;
-echo '1';
+
 $user_id = $_SESSION['admin_id'];
-// if(isset($user_id)&&!empty($user_id)){
-// 	$arr_file_types = ['image/png', 'image/jpg', 'image/jpeg'];
-//
-//
-// 	if ($_FILES['file']['size'] < 5*MB){
-//
-// 	if (!file_exists('../../../../JamFichiers/Img/ImagesDuSite/Original')) {
-// 	    mkdir('Original', 0777);
-// 	}
-//
-//
-// 	$imagenouvelle = rand(100, 10000) . $_FILES['file']['name'];
-// 	move_uploaded_file($_FILES['file']['tmp_name'], '../../../../JamFichiers/Img/ImagesDuSite/Original/' . $imagenouvelle);
-//
-//
-// 	echo "success";
-// 	die();
-// }}else{
-// 	die();
-// }
+if(isset($user_id)&&!empty($user_id)){
+	$arr_file_types = ['image/png', 'image/jpg', 'image/jpeg'];
+
+
+	if ($_FILES['file']['size'] < 5*MB){
+
+	if (!file_exists('../../../../JamFichiers/Img/ImagesDuSite/Original')) {
+	    mkdir('Original', 0777);
+	}
+
+
+	$imagenouvelle = rand(100, 10000) . $_FILES['file']['name'];
+	move_uploaded_file($_FILES['file']['tmp_name'], '../../../../JamFichiers/Img/ImagesDuSite/Original/' . $imagenouvelle);
+
+
+	echo "success";
+	die();
+}}else{
+	die();
+}
 ?>
