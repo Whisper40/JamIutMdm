2c4ab92e3b53f142dd65ebeafafcd32ec76b896d
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-06-02 20:50:11 +0200

    Update addimage.php

--- administration/ajax/addimage.php
+++ administration/ajax/addimage.php
@@ -9,16 +9,17 @@ $test = $_POST['catimage'];
 
 $user_id = $_SESSION['admin_id'];
 if(isset($user_id)&&!empty($user_id)){
+   echo '0';
 	$arr_file_types = ['image/png', 'image/jpg', 'image/jpeg'];
 
 
 	if ($_FILES['file']['size'] < 5*MB){
-
+ echo '1';
 	if (!file_exists('../../../../JamFichiers/Img/ImagesDuSite/Original/')) {
 	    mkdir('Original', 0777);
 	}
 
-
+ echo '2';
 	$imagenouvelle = rand(100, 10000) . $_FILES['file']['name'];
 	move_uploaded_file($_FILES['file']['tmp_name'], '../../../../JamFichiers/Img/ImagesDuSite/Original/' . $imagenouvelle);
 
