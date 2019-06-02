b7182553f7c8e4814a57670109bad6b55e532fde
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-06-02 15:27:50 +0200

    Update formconnection.php

--- administration/ajax/formconnection.php
+++ administration/ajax/formconnection.php
@@ -84,7 +84,7 @@ echo '3';
                     )
                 );
 
-
+echo '3.5';
                 $update = $db->prepare("UPDATE admin SET last_connect=:date, datesystem=:datesystem WHERE id=:id");
                 $update->execute(array(
                     "date"=>$date,
