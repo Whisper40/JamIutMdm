80c3e40ae2b68f5771f066f8efaf023bd7c7496b
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2019-06-13 18:33:05 +0200

    Update modifdespages.php

--- administration/modifdespages.php
+++ administration/modifdespages.php
@@ -4294,7 +4294,7 @@ if ($uploadOk == 0) {
             $typeactivite = $_POST['typeactivite'];
             $slug = slugify($title);
 
-            if (stripos($slug, $typeactivite) !== FALSE){
+            if (stripos($slug, $typeactivite) !== TRUE){
               if (($typeactivite == "sportive") || ($typeactivite == "nettoyage") || ($typeactivite == "orientation") || ($typeactivite == "soireebar")){
                 $payant = '0';
               }else{
