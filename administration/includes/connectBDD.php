cfd8530cd5ba715373ee2867815b4ef13c48698b
Mathis Grisel <38182562+GRY64@users.noreply.github.com>
2019-07-24 23:33:36 +0200

    Retour Arrière
    Retour Arrière

--- administration/includes/connectBDD.php
+++ administration/includes/connectBDD.php
@@ -3,7 +3,7 @@ ini_set("session.cookie_domain", ".jam-mdm.fr");
 session_start();
 
 try{
-d
+
 	$db = new PDO('mysql:host=127.0.0.1;dbname=siteassociation', 'admin','ENTRERVOTREMDP');
 	$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); // les noms de champs seront en caractères minuscules
 	$db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION); // les erreurs lanceront des exceptions
